d740f6bc79e36c9adf93d42af5477242e2da896e
Gonçalo Silva and Scott Andrews <pair+gonalo+sandrews@pivotallabs.com>
2013-06-13 12:13:19 +0100

    Adding logging

--- src/main/java/org/springframework/site/guides/GitHubGettingStartedService.java
+++ src/main/java/org/springframework/site/guides/GitHubGettingStartedService.java
@@ -1,5 +1,6 @@
 package org.springframework.site.guides;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.codec.Base64;
 import org.springframework.social.github.api.GitHub;
@@ -13,6 +14,8 @@ public class GitHubGettingStartedService implements GettingStartedService {
 
 	private static final String URL = "https://api.github.com/repos/springframework-meta/gs-{guideId}/contents/README.src.md";
 
+	private static final Logger log = Logger.getLogger(GitHubGettingStartedService.class);
+
 	private final GitHub gh;
 
 	@Autowired
@@ -23,11 +26,14 @@ public class GitHubGettingStartedService implements GettingStartedService {
 	@Override
 	public String loadGuide(String guideId) {
 		try {
+			log.info(String.format("Fetching getting started guide for '%s'", guideId));
 			Map<String, String> readme = gh.restOperations().getForObject(URL, Map.class, guideId);
 			return new String(Base64.decode(readme.get("content").getBytes()));
 		}
 		catch (RestClientException e) {
-			throw new GuideNotFoundException(String.format("No getting started guide found for '%s'", guideId), e);
+			String msg = String.format("No getting started guide found for '%s'", guideId);
+			log.warn(msg, e);
+			throw new GuideNotFoundException(msg, e);
 		}
 	}
 
