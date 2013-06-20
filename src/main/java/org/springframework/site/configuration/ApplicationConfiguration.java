3ece5553a0c25720046e09f8cd02491d443795b1
Dave Syer and Nicholas Street <pair+dsyer+nstreet@pivotallabs.com>
2013-06-20 17:34:01 +0100

    Sign in with github, required to access /admin/**
    - The user is not automatically forwarded to the originally requested URL yet
    - We do not check the organization membership yet
    
    [#51798817]

--- src/main/java/org/springframework/site/configuration/ApplicationConfiguration.java
+++ src/main/java/org/springframework/site/configuration/ApplicationConfiguration.java
@@ -31,20 +31,38 @@ import org.springframework.bootstrap.context.annotation.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.web.HttpConfiguration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.site.documentation.DocumentationService;
+import org.springframework.social.connect.Connection;
+import org.springframework.social.connect.ConnectionSignUp;
+import org.springframework.social.connect.mem.InMemoryUsersConnectionRepository;
+import org.springframework.social.connect.support.ConnectionFactoryRegistry;
+import org.springframework.social.connect.web.ProviderSignInController;
+import org.springframework.social.connect.web.SignInAdapter;
 import org.springframework.social.github.api.GitHub;
 import org.springframework.social.github.api.impl.GitHubTemplate;
+import org.springframework.social.github.connect.GitHubConnectionFactory;
 import org.springframework.util.Assert;
+import org.springframework.web.context.request.NativeWebRequest;
 
 @EnableAutoConfiguration
 @Configuration
 @ComponentScan(basePackages = "org.springframework.site")
 public class ApplicationConfiguration {
-	
-	private static final Log logger = LogFactory.getLog(ApplicationConfiguration.class);
-	
+
+	private static final Log logger = LogFactory
+			.getLog(ApplicationConfiguration.class);
+
 	@Autowired
 	private DocumentationService documentationService;
 
@@ -53,9 +71,18 @@ public class ApplicationConfiguration {
 	}
 
 	public static SpringApplication build() {
-		SpringApplication application = new SpringApplication(ApplicationConfiguration.class);
-		application.setDefaultCommandLineArgs("--spring.template.mode=LEGACYHTML5", "--spring.template.cache=false");
+		SpringApplication application = new SpringApplication(
+				ApplicationConfiguration.class);
+		application.setDefaultCommandLineArgs(
+				"--spring.template.mode=LEGACYHTML5",
+				"--spring.template.cache=false");
 		return application;
+	}
+
+	@Bean
+	public GitHubConnectionFactory gitHubConnectionFactory() {
+		return new GitHubConnectionFactory("fb06c006c2ed62fe9e8b",
+				"164264e3f6d70c7c21713b7fa64225cb8d6107b2");
 	}
 
 	@Bean
@@ -63,16 +90,82 @@ public class ApplicationConfiguration {
 		// TODO parametrize auth token
 		return new GitHubTemplate("5a0e089d267693b45926d7f620d85a2eb6a85da6");
 	}
-	
+
+	@Configuration
+	@Order(Integer.MAX_VALUE-1)
+	protected static class SigninAuthenticationConfiguration extends
+			WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpConfiguration http) throws Exception {
+			http.antMatcher("/signin/github").authorizeUrls().anyRequest()
+					.anonymous();
+		}
+	}
+
+	@Configuration
+	@Order(Integer.MAX_VALUE)
+	protected static class AdminAuthenticationConfiguration extends
+			WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpConfiguration http) throws Exception {
+			http.exceptionHandling().authenticationEntryPoint(
+					authenticationEntryPoint());
+			http.antMatcher("/admin/**").authorizeUrls().anyRequest()
+					.authenticated();
+		}
+
+		private AuthenticationEntryPoint authenticationEntryPoint() {
+			// TODO: can we POST back here?
+			return new LoginUrlAuthenticationEntryPoint("/signin");
+		}
+
+		@Bean
+		public ProviderSignInController providerSignInController(
+				GitHubConnectionFactory connectionFactory) {
+			ConnectionFactoryRegistry registry = new ConnectionFactoryRegistry();
+			registry.addConnectionFactory(connectionFactory);
+			InMemoryUsersConnectionRepository repository = new InMemoryUsersConnectionRepository(
+					registry);
+			repository.setConnectionSignUp(new ConnectionSignUp() {
+				@Override
+				public String execute(Connection<?> connection) {
+					return connection.getDisplayName() != null ? connection
+							.getDisplayName() : null;
+				}
+			});
+			return new ProviderSignInController(registry, repository,
+					new SignInAdapter() {
+						@Override
+						public String signIn(String userId,
+								Connection<?> connection,
+								NativeWebRequest request) {
+							// TODO: get group info from github and determine
+							// role
+							Authentication authentication = new UsernamePasswordAuthenticationToken(
+									userId,
+									"N/A",
+									AuthorityUtils
+											.commaSeparatedStringToAuthorityList("ROLE_USER"));
+							SecurityContextHolder.getContext()
+									.setAuthentication(authentication);
+							return null;
+						}
+					});
+		}
+	}
+
 	@PostConstruct
 	public void loadDocumentationProjects() {
 		bind("documentation.yml", documentationService);
 	}
 
-	public static void bind(String path, DocumentationService documentationService) {
+	public static void bind(String path,
+			DocumentationService documentationService) {
 		RelaxedDataBinder binder = new RelaxedDataBinder(documentationService);
 		YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
-		factory.setResources(new Resource[] {new ClassPathResource(path)});
+		factory.setResources(new Resource[] { new ClassPathResource(path) });
 		Properties properties = factory.getObject();
 		logger.info("Binding properties: " + properties);
 		properties.remove("projects");
@@ -82,7 +175,8 @@ public class ApplicationConfiguration {
 			}
 		}
 		binder.bind(new MutablePropertyValues(properties));
-		Assert.state(!binder.getBindingResult().hasErrors(), "Errors binding " + path + ": " + binder.getBindingResult().getAllErrors());
+		Assert.state(!binder.getBindingResult().hasErrors(), "Errors binding "
+				+ path + ": " + binder.getBindingResult().getAllErrors());
 	}
 
 }
--- src/test/java/org/springframework/site/integration/BasicAcceptanceTests.java
+++ src/test/java/org/springframework/site/integration/BasicAcceptanceTests.java
@@ -1,6 +1,10 @@
 package org.springframework.site.integration;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
@@ -9,6 +13,9 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -76,4 +83,15 @@ public class BasicAcceptanceTests {
 		return restTemplate;
 	}
 
+	@Test
+	public void adminIsSecure() {
+		ResponseEntity<String> response = getRestTemplate().getForEntity(
+				"http://localhost:8080/admin", String.class);
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+
+		Document html = Jsoup.parse(response.getBody());
+		Element loginButton = html.select("form button").first();
+		assertThat("No login button found", loginButton, is(notNullValue()));
+		assertThat(loginButton.text(), is(equalTo("Sign in with github")));
+	}
 }
