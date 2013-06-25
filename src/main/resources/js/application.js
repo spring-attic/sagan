32d18fac8f4dab422801946b9d9ea1bac8c4b87e
Gonçalo Silva and Nicholas Street <pair+gsilva+nstreet@pivotallabs.com>
2013-06-25 12:28:17 +0100

    Disabling filtering of *.swf by maven.
    - Also, not showing Copy button if flash is not available
    
    [#51965033]

--- src/main/resources/js/application.js
+++ src/main/resources/js/application.js
@@ -15,15 +15,17 @@ $(function(){
     $('.js-spring-popover').springPopover();
 });
 
-ZeroClipboard.setDefaults( { moviePath: 'http://sagan.cfapps.io/js/ZeroClipboard.swf' } );
+ZeroClipboard.setDefaults( { moviePath: '/js/ZeroClipboard.swf' } );
 
 $(document).ready(function() {
-    $('article .highlight pre').each(function(index) {
-            var codeBlockId = "code-block-"+ index;
-            $(this).attr('id', codeBlockId);
-            var button = $('<button class="copy-button" id="copy-button-"' + index + ' data-clipboard-target="' + codeBlockId + '" title="Click to copy to clipboard.">Copy</button>');
-            $(this).before(button);
-            new ZeroClipboard(button);
-        }
-    );
+    if (ZeroClipboard.detectFlashSupport()) {
+        $('article .highlight pre').each(function(index) {
+                var codeBlockId = "code-block-"+ index;
+                $(this).attr('id', codeBlockId);
+                var button = $('<button class="copy-button" id="copy-button-"' + index + ' data-clipboard-target="' + codeBlockId + '" title="Click to copy to clipboard.">Copy</button>');
+                $(this).before(button);
+                new ZeroClipboard(button);
+            }
+        );
+    }
 });
