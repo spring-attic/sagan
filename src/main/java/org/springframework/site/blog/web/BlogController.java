ac31be267deea59f61474e2f5c9e1dbbf06a00d2
Kurtis Seebaldt <pair+kurtis@pivotallabs.com>
2013-06-28 10:22:15 +0100

    Show blog categories on an individual blog post
    - extract post categories into a shared fragment
    - use categories fragment on index and show pages

--- src/main/java/org/springframework/site/blog/web/BlogController.java
+++ src/main/java/org/springframework/site/blog/web/BlogController.java
@@ -32,6 +32,7 @@ public class BlogController {
 	@RequestMapping(value = "/{postId:[0-9]+}{slug:.*}", method = { GET, HEAD })
 	public String showPost(@PathVariable Long postId, @PathVariable String slug, Model model) {
 		model.addAttribute("post", service.getPublishedPost(postId));
+		model.addAttribute("categories", PostCategory.values());
 		return "blog/show";
 	}
 
