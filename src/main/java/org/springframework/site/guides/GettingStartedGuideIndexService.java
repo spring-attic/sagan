78a1c4dd3cffa3f620d4ce2509e1dbaba5a22af9
Dave Syer and Gonçalo Silva <pair+dsyer+gonalo@pivotallabs.com>
2013-07-04 09:46:47 +0100

    Skipping indexing getting started guides that can't be found

--- src/main/java/org/springframework/site/guides/GettingStartedGuideIndexService.java
+++ src/main/java/org/springframework/site/guides/GettingStartedGuideIndexService.java
@@ -3,6 +3,8 @@ package org.springframework.site.guides;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.bootstrap.actuate.metrics.CounterService;
+import org.springframework.bootstrap.actuate.metrics.GaugeService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.site.search.SearchService;
 import org.springframework.stereotype.Service;
@@ -19,6 +21,12 @@ public class GettingStartedGuideIndexService {
 	private final GettingStartedService gettingStartedService;
 
 	@Autowired
+	private CounterService counters;
+
+	@Autowired
+	private GaugeService gauges;
+
+	@Autowired
 	public GettingStartedGuideIndexService(SearchService searchService, GettingStartedService gettingStartedService) {
 		this.searchService = searchService;
 		this.gettingStartedService = gettingStartedService;
@@ -28,11 +36,29 @@ public class GettingStartedGuideIndexService {
 	@Scheduled(fixedDelay = ONE_HOUR, initialDelayString = "${guides.index.delay:600000}")
 	public void indexGuides() {
 		logger.info("Indexing getting started guides");
+		int count = 0;
 		for (GuideRepo repo : gettingStartedService.listGuides()) {
-			// TODO: optimize fetch of guide content (if github supports it)?
-			// TODO: only index the new ones if possible
-			GettingStartedGuide guide = gettingStartedService.loadGuide(repo.getGuideId());
-			searchService.saveToIndex(mapper.map(guide));
+			try {
+				// TODO: optimize fetch of guide content (if github supports it)?
+				// TODO: only index the new ones if possible
+				GettingStartedGuide guide = gettingStartedService.loadGuide(repo.getGuideId());
+				searchService.saveToIndex(mapper.map(guide));
+				count++;
+				if (gauges != null) {
+					gauges.set("search.indexes.guides.processed", count);
+				}
+			} catch (Exception e) {
+				if (counters != null) {
+					counters.increment("search.indexes.guides.errors.count");
+
+				}
+				logger.warn("Unable to load getting started guide: " + repo.getGuideId() + "(" + e.getClass().getName() + ", " + e.getMessage() + ")");
+				continue;
+			}
+		}
+		if (counters != null) {
+			counters.increment("search.indexes.guides.refresh.count");
 		}
 	}
+
 }
---
+++ src/test/java/org/springframework/site/guides/GettingStartedGuideIndexServiceTest.java
@@ -0,0 +1,48 @@
+package org.springframework.site.guides;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.site.search.SearchEntry;
+import org.springframework.site.search.SearchService;
+import org.springframework.web.client.RestClientException;
+
+import java.util.ArrayList;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+
+public class GettingStartedGuideIndexServiceTest {
+
+	private SearchService searchService = mock(SearchService.class);
+	private GettingStartedService gettingStartedService = mock(GettingStartedService.class);
+	private GettingStartedGuideIndexService service;
+
+	@Before
+	public void setUp() throws Exception {
+		ArrayList<GuideRepo> repos = new ArrayList<GuideRepo>();
+		GuideRepo repo = new GuideRepo();
+		repo.setName("gs-awesome-guide");
+		repos.add(repo);
+		when(gettingStartedService.listGuides()).thenReturn(repos);
+		service = new GettingStartedGuideIndexService(searchService, gettingStartedService);
+	}
+
+	@Test
+	public void savesGuidesToSearchIndex() throws Exception {
+		GettingStartedGuide guide = new GettingStartedGuide("awesome-guide", "some content", "some sidebar");
+		when(gettingStartedService.loadGuide(anyString())).thenReturn(guide);
+		service.indexGuides();
+		verify(searchService).saveToIndex(any(SearchEntry.class));
+	}
+
+	@Test
+	public void skipsGuidesNotFound() throws Exception {
+		when(gettingStartedService.loadGuide(anyString())).thenThrow(RestClientException.class);
+		service.indexGuides();
+		verifyZeroInteractions(searchService);
+	}
+}
