74bad294d295e72f15408080875a12ac936b08c0
Dave Syer <dsyer@vmware.com>
2013-07-09 16:40:30 +0100

    Switch to executable-jar packaging

---
+++ src/main/resources/site.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+CP=.
+for f in lib/*.jar; do
+  CP=$CP:"$f"
+done
+
+java -Xmx1260m -XX:MaxPermSize=512m -classpath "$CP" org.springframework.site.configuration.ApplicationConfiguration $*
