d7a97d328bb3af6aef491677ab365487893d3261
Dave Syer <dsyer@vmware.com>
2013-07-17 09:54:45 +0100

    Revert change to indexer.sh

--- src/main/resources/indexer.sh
+++ src/main/resources/indexer.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-java -Xmx1260m -XX:MaxPermSize=512m -classpath "." org.springframework.launcher.JarLauncher $*
+CP=.
+for f in lib/*.jar; do
+  CP=$CP:"$f"
+done
+
+java -Xmx1260m -XX:MaxPermSize=512m -classpath "$CP" org.springframework.site.indexer.configuration.IndexerConfiguration $*
