39d44d68ab38c659f454e00bfadf3b1c3d000721
Gonçalo Silva and Kurtis Seebaldt <pair+gonalo+kurtis@pivotallabs.com>
2013-07-17 09:37:48 +0100

    reverting run.sh so src/main/resources are loaded first in the classpath

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git pull --rebase
 mvn clean compile
+classpath=$(mvn exec:exec -Dexec.executable="echo" -Dexec.args="CLASSPATH %classpath" | grep "^CLASSPATH" | cut -d" " -f2)
 mvn sass:watch &
-mvn exec:java -Dexec.main="org.springframework.site.web.configuration.ApplicationConfiguration"
+mvn exec:exec -Dexec.executable="java" -Dexec.args="-classpath src/main/resources:$classpath org.springframework.site.web.configuration.ApplicationConfiguration"
