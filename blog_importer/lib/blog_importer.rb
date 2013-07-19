cbf577f40942449000aaef82677bcf2dddcb7dbb
Gonçalo Silva and Jesse Heitler <pair+gsilva+jheitler@pivotallabs.com>
2013-07-19 14:58:45 +0100

    Refactoring the blog importer
    - using site endpoint 'migrate/blogpost' instead of direct access to the DB

--- blog_importer/lib/blog_importer.rb
+++ blog_importer/lib/blog_importer.rb
@@ -10,6 +10,10 @@ class SiteApi
   def save_member_profile(options)
     HTTParty.post("http://#{@base_uri}/migration/profile", {body: options})
   end
+
+  def save_blog_post(options)
+    HTTParty.post("http://#{@base_uri}/migration/blogpost", {body: options})
+  end
 end
 
 class BlogImporter
@@ -18,14 +22,7 @@ class BlogImporter
     @site_api = site_api
   end
 
-  def import!
-    import(true)
-  end
-
-  def import(force=false)
-    return false unless force || user_happy_to_continue
-
-    truncate_post_table!
+  def import
     import_authors
     import_posts
   end
@@ -34,10 +31,10 @@ class BlogImporter
     authors = xml_doc.xpath("//wp:author")
     authors.each do |author|
       data = {
-        memberId: author.xpath('wp:author_login').text,
-        githubUsername: author.xpath('wp:author_login').text,
-        gravatarEmail: author.xpath('wp:author_email').text,
-        name: author.xpath('wp:author_display_name').text
+          memberId: author.xpath('wp:author_login').text,
+          githubUsername: author.xpath('wp:author_login').text,
+          gravatarEmail: author.xpath('wp:author_email').text,
+          name: author.xpath('wp:author_display_name').text
       }
       @site_api.save_member_profile data
     end
@@ -46,20 +43,19 @@ class BlogImporter
   def import_posts
     puts "Importing #{@filename} "
     post_elements = xml_doc.xpath("//item[wp:status/text()='publish'][wp:post_type/text()='post']")
-    post_elements.each_with_index do |element, i|
+    post_elements.each do |element|
       print_progress
-      title = element.xpath('title').text
       post_date = element.xpath('wp:post_date_gmt').text
-      content = element.xpath('content:encoded').text
+      data = {
+          title: element.xpath('title').text,
+          content: element.xpath('content:encoded').text,
+          category: 'ENGINEERING',
+          createdAt: post_date,
+          publishAt: post_date,
+          authorMemberId: element.xpath('dc:creator').text
+      }
 
-      query = <<-EOS
-        INSERT INTO post (id, broadcast, category, createdat, draft, publishat, rawcontent, renderedcontent, renderedsummary, title, author_id)
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
-      EOS
-
-      values = [i, false, 'ENGINEERING', post_date, false, post_date,
-                content, content, content[0...500], title, 1]
-      conn.exec(query, values)
+      @site_api.save_blog_post data
     end
     true
   end
@@ -69,14 +65,6 @@ class BlogImporter
     $stdout.flush
   end
 
-  def user_happy_to_continue
-    puts "This will remove all existing posts from the database. Enter 'yes' to continue"
-    gets.chomp == 'yes'
-  end
-
-  def truncate_post_table!
-    conn.exec("TRUNCATE post")
-  end
 
   def xml_doc
     @xml_doc ||= File.open(@filename) do |f|
@@ -86,14 +74,6 @@ class BlogImporter
 
   def blog_file_name
     File.exists?(FULL_BLOG_FILE) ? FULL_BLOG_FILE : TEST_BLOG_FILE
-  end
-
-  def conn
-    @conn ||= PG.connect(dbname: 'blog_import')
-
-    # The cloudfoundry development db
-    #@conn ||= PG::Connection.new({"dbname"=>"zbgubjyl", "host"=>"babar.elephantsql.com", "port"=>5432, "user"=>"zbgubjyl", "password"=>"BOHcUgFDTIPJ8KxCX9tT3w0sZct5WyCR"})
-
   end
 end
 
--- blog_importer/spec/blog_importer_spec.rb
+++ blog_importer/spec/blog_importer_spec.rb
@@ -5,17 +5,41 @@ require 'httparty'
 
 describe SiteApi do
 
+  member_id = 'yada'
+  post_title = 'this is a test blog post'
+  db = PG.connect(dbname: 'blog_import')
+
+  before do
+    db.exec("DELETE FROM post WHERE title = '#{post_title}'")
+    db.exec("DELETE FROM memberprofile WHERE memberid = '#{member_id}'")
+  end
+
+  after do
+    db.exec("DELETE FROM post WHERE title = '#{post_title}'")
+    db.exec("DELETE FROM memberprofile WHERE memberid = '#{member_id}'")
+  end
+
   it "saves a member profile" do
-    db = PG.connect(dbname: 'blog_import')
-    db.exec("DELETE FROM memberprofile WHERE memberid = 'yada'")
-    db.exec("SELECT count(*) FROM memberprofile WHERE memberid = 'yada'") do |result|
-      result.getvalue(0, 0).to_i.should == 0
+    api = SiteApi.new('localhost:8080')
+    api.save_member_profile(memberId: member_id)
+
+    db.exec("SELECT count(*) FROM memberprofile WHERE memberid = '#{member_id}'") do |result|
+      result.getvalue(0, 0).to_i.should == 1
     end
+  end
+
+  it "saves a blog post" do
+    db.exec("INSERT INTO memberprofile (memberid, id) VALUES ('#{member_id}',130303)")
 
     api = SiteApi.new('localhost:8080')
-    api.save_member_profile(memberId: 'yada')
+    api.save_blog_post(title: post_title,
+                       content: 'this is a blog post',
+                       category: 'ENGINEERING',
+                       publishAt: "2000-01-01 00:00",
+                       createdAt: "1999-01-01 00:00",
+                       authorMemberId: member_id,)
 
-    db.exec("SELECT count(*) FROM memberprofile WHERE memberid = 'yada'") do |result|
+    db.exec("SELECT count(*) FROM post WHERE title = '#{post_title}'") do |result|
       result.getvalue(0, 0).to_i.should == 1
     end
   end
@@ -24,97 +48,62 @@ end
 
 describe BlogImporter do
 
-  test_files = [{ filename: "test_blog_export.xml",
-                  expected_post_count: 2 ,
-                  expected_author_count: 2,
-                  expected_author: {
-                      memberId: 'sample',
-                      githubUsername: 'sample',
-                      gravatarEmail: 'sample@springsource.com',
-                      name: 'Mr Sample'
-                  }
-                } ]
+  let(:double_siteapi) { double('siteapi').as_null_object }
+  let(:importer) { BlogImporter.new(xml_filename, double_siteapi) }
 
-  if File.exists?("full_blog_export.xml")
-    test_files << { filename: "full_blog_export.xml",
-                    expected_post_count: 525,
-                    expected_author_count: 86,
-                    expected_author: {
-                        memberId: 'benh',
-                        githubUsername: 'benh',
-                        gravatarEmail: 'bhale@vmware.com',
-                        name: 'Ben Hale'
-                    }
-                  }
+  context "After importing test xml file" do
+    let(:xml_filename) { "test_blog_export.xml" }
+
+    it "creates new memberProfiles for an author" do
+      double_siteapi.should_receive('save_member_profile').exactly(3).times
+      importer.import
+    end
+
+    it "creates memberProfiles with the correct info" do
+      expected_author = {
+          memberId: 'sample',
+          githubUsername: 'sample',
+          gravatarEmail: 'sample@springsource.com',
+          name: 'Mr Sample'
+      }
+      double_siteapi.should_receive('save_member_profile').with(expected_author)
+      importer.import
+    end
+
+    it "creates a post for every published post in the import file" do
+      double_siteapi.should_receive('save_blog_post').exactly(2).times
+      importer.import
+    end
+
+    it "creates a post with the correct info" do
+      expected_post = {
+          title: 'Another Reason to Love Spring 2.0: Interceptor Combining',
+          content: 'Interceptor Combining is out of this world!',
+          category: 'ENGINEERING',
+          publishAt: '2006-04-09 20:41:19',
+          createdAt: '2006-04-09 20:41:19',
+          authorMemberId: 'sample',
+      }
+      double_siteapi.should_receive('save_blog_post').with(expected_post)
+      importer.import
+    end
+
   end
 
-  test_files.each do |test_context|
-
-
-    context "After importing #{test_context[:filename]}" do
-
-      let(:double_siteapi) { double('siteapi').as_null_object }
-
-      let(:db) { PG.connect(dbname: 'blog_import') }
-
-      let(:importer) { BlogImporter.new(test_context[:filename], double_siteapi) }
-
-      let(:expected_post_count) { test_context[:expected_post_count] }
-      let(:expected_author_count) { test_context[:expected_author_count] }
-      let(:expected_author) { test_context[:expected_author] }
-
+  if File.exists?("full_blog_export.xml")
+    context "After importing full xml file" do
+      let(:xml_filename) { "full_blog_export.xml" }
 
       it "creates new memberProfiles for an author" do
-        double_siteapi.should_receive('save_member_profile').exactly(expected_author_count).times
-        importer.import!
-      end
-
-      it "creates memberProfiles with the correct info" do
-        double_siteapi.should_receive('save_member_profile').with(expected_author)
-        importer.import!
-      end
-
-      it "truncates the post table before performing an import" do
-        importer.import!
-        importer.import!
-        db.exec("SELECT count(*) FROM post") do |result|
-          result.getvalue(0, 0).to_i.should == expected_post_count
-        end
+        double_siteapi.should_receive('save_member_profile').exactly(86).times
+        importer.import
       end
 
       it "creates a post for every published post in the import file" do
-        importer.import!
-        db.exec("SELECT count(*) FROM post") do |result|
-          result.getvalue(0, 0).to_i.should == expected_post_count
-        end
+        double_siteapi.should_receive('save_blog_post').exactly(525).times
+        importer.import
       end
-
-      context "first post in the database" do
-        subject do
-          importer.import!
-          db.exec("SELECT * FROM post ORDER BY id ASC LIMIT 1")[0]
-        end
-
-        its(['title']) { should == "Spring 2.0's JMS Improvements" }
-        its(['broadcast']) { should == "f" }
-        its(['category']) { should == "ENGINEERING" }
-        its(['createdat']) { should == "2006-04-09 20:22:43" }
-        its(['draft']) { should == "f" }
-        its(['publishat']) { should == "2006-04-09 20:22:43" }
-        its(['rawcontent']) { should start_with("With the release of Spring 1.1 the Spring community was given") }
-        its(['renderedcontent']) { should start_with("With the release of Spring 1.1 the Spring community was given") }
-        its(['renderedsummary']) { should start_with("With the release of Spring 1.1 the Spring community was given") }
-        its(['author_id']) { should == "1" }
-
-        it "has a shortened summary" do
-          subject['renderedsummary'].length.should <= 500
-          # TODO end with word
-        end
-
-      end
-
     end
-
   end
 
 end
