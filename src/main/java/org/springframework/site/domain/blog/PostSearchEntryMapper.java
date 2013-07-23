d7e65fa10fe0955b96a8164e5014f12baadcc73b
Jesse Heitler and Kurtis Seebaldt <pair+jheitler+kurtis@pivotallabs.com>
2013-07-23 11:53:52 +0100

    strip html tags from all indexed content.  Show highlighted summaries

--- src/main/java/org/springframework/site/domain/blog/PostSearchEntryMapper.java
+++ src/main/java/org/springframework/site/domain/blog/PostSearchEntryMapper.java
@@ -1,5 +1,6 @@
 package org.springframework.site.domain.blog;
 
+import org.jsoup.Jsoup;
 import org.springframework.site.search.SearchEntry;
 import org.springframework.site.search.SearchEntryMapper;
 
@@ -9,8 +10,12 @@ public class PostSearchEntryMapper implements SearchEntryMapper<Post> {
 	public SearchEntry map(Post post) {
 		SearchEntry entry = new SearchEntry();
 		entry.setTitle(post.getTitle());
-		entry.setSummary(post.getRenderedSummary());
-		entry.setRawContent(post.getRawContent());
+
+		String summary = Jsoup.parse(post.getRenderedSummary()).text();
+		String content = Jsoup.parse(post.getRenderedContent()).text();
+
+		entry.setSummary(summary);
+		entry.setRawContent(content);
 		//TODO this should be encapsulated somewhere else
 		entry.setPath("/blog/" + post.getSlug());
 		entry.setPublishAt(post.getPublishAt());
--- src/main/java/org/springframework/site/web/guides/GuideSearchEntryMapper.java
+++ src/main/java/org/springframework/site/web/guides/GuideSearchEntryMapper.java
@@ -1,5 +1,6 @@
 package org.springframework.site.web.guides;
 
+import org.jsoup.Jsoup;
 import org.springframework.site.domain.guides.GettingStartedGuide;
 import org.springframework.site.search.SearchEntry;
 import org.springframework.site.search.SearchEntryMapper;
@@ -12,9 +13,11 @@ public class GuideSearchEntryMapper implements SearchEntryMapper<GettingStartedG
 	public SearchEntry map(GettingStartedGuide guide) {
 		SearchEntry entry = new SearchEntry();
 		entry.setTitle(guide.getGuideId());
-		// TODO: summary should be generated after a search matches, not statically here (so this is purely a hack for now)
-		entry.setSummary(guide.getContent().substring(0, Math.min(500, guide.getContent().length())));
-		entry.setRawContent(guide.getContent());
+
+		String text = Jsoup.parse(guide.getContent()).text();
+
+		entry.setSummary(text.substring(0, Math.min(500, text.length())));
+		entry.setRawContent(text);
 		entry.setPath(GettingStartedController.getPath(guide));
 		// TODO: Can we get a publish date form github?
 		entry.setPublishAt(new Date(0L));
