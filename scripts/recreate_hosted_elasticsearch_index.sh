6434ebd51fec893a92a43ba0171d326d8492f054
Jesse Heitler and Kurtis Seebaldt <pair+jheitler+kurtis@pivotallabs.com>
2013-07-24 10:35:10 +0100

    Script to recreate the hosted index.

---
+++ scripts/recreate_hosted_elasticsearch_index.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+echo "\nDelete the existing index."
+curl -XDELETE 'https://qlmsjwfa.api.qbox.io/site'
+
+echo "\nCreate the index again."
+curl -X PUT http://qlmsjwfa.api.qbox.io/site -d '{ "number_of_shards" : 1, "number_of_replicas" : 0}'
+
+echo "\nInstall the settings, requires closing the index first."
+curl -XPOST 'http://qlmsjwfa.api.qbox.io/site/_close'
+curl -XPUT 'http://qlmsjwfa.api.qbox.io/site/_settings' -d @../src/main/resources/config/elasticsearch/settings.json 
+curl -XPOST 'http://qlmsjwfa.api.qbox.io/site/_open'
+
+echo "\nCheck the settings."
+curl http://qlmsjwfa.api.qbox.io/site/_settings?pretty=true
+
+echo "\nInstall the mapping - do once per type/mapping file."
+curl -XPUT 'http://qlmsjwfa.api.qbox.io/site/site/_mappings' -d @../src/main/resources/config/elasticsearch/mappings/site.json 
+
+echo "\nCheck the mappings."
+curl http://qlmsjwfa.api.qbox.io/site/site/_mappings?pretty=true
