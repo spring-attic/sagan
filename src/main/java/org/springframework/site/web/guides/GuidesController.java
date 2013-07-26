c8309e99d69e77732220cb271e2520082d4d674f
Gonçalo Silva and Kurtis Seebaldt <pair+gsilva+kurtis@pivotallabs.com>
2013-07-26 17:49:14 +0100

    load list of guides in top level guide controller

--- src/main/java/org/springframework/site/web/guides/GuidesController.java
+++ src/main/java/org/springframework/site/web/guides/GuidesController.java
@@ -1,7 +1,10 @@
 package org.springframework.site.web.guides;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.site.domain.guides.GettingStartedService;
 import org.springframework.site.web.NavSection;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -12,8 +15,16 @@ import static org.springframework.web.bind.annotation.RequestMethod.HEAD;
 @NavSection("guides")
 public class GuidesController {
 
+	private GettingStartedService service;
+
+	@Autowired
+	public GuidesController(GettingStartedService service) {
+		this.service = service;
+	}
+
 	@RequestMapping(value = "", method = { GET, HEAD })
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("guides", service.listGuides());
 		return "guides/index";
 	}
 }
