fffe175c92167d887bc445838a524d93ee6ed483
pivotal <nicholas.street@gmail.com>
2013-08-01 12:59:08 +0100

    Tweaking the architecture dialog display name in /tools/sts/all & /tools/ggts/all

--- src/main/java/org/springframework/site/domain/tools/toolsuite/Architecture.java
+++ src/main/java/org/springframework/site/domain/tools/toolsuite/Architecture.java
@@ -1,8 +1,11 @@
 package org.springframework.site.domain.tools.toolsuite;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Architecture {
+	private static final Pattern NAME_PATTERN = Pattern.compile("\\(([a-zA-Z]+)");
 	private String name;
 	private List<DownloadLink> downloadLinks;
 
@@ -17,5 +20,14 @@ public class Architecture {
 
 	public List<DownloadLink> getDownloadLinks() {
 		return downloadLinks;
+	}
+
+	public String getDisplayName() {
+		Matcher matcher = NAME_PATTERN.matcher(name);
+		String arch = "WIN";
+		if (matcher.find()) {
+			arch = matcher.group(1);
+		}
+		return String.format("%s, %sBIT", arch.toUpperCase(), downloadLinks.get(0).getArchitecture());
 	}
 }
