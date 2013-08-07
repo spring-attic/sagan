c5ea6beba986586fa9095703b76ab096a198f5b0
Dave Syer <dsyer@gopivotal.com>
2013-08-07 16:36:44 +0100

    Explicit exclude for JPA config in redirector

--- src/main/java/org/springframework/redirector/RedirectorConfiguration.java
+++ src/main/java/org/springframework/redirector/RedirectorConfiguration.java
@@ -5,12 +5,15 @@ import java.io.InputStream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = { HibernateJpaAutoConfiguration.class,
+		JpaRepositoriesAutoConfiguration.class })
 @Configuration
 @ComponentScan(basePackages = "org.springframework.redirector")
 public class RedirectorConfiguration {
