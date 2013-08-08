5784598f93f0916668eee07a69ff31ad1f063ef8
Gonçalo Silva and Jeff Taggart <pair+gsilva+jtaggart@pivotallabs.com>
2013-08-08 18:47:33 +0100

    remove poor man debugging

--- src/main/resources/main.sh
+++ src/main/resources/main.sh
@@ -3,7 +3,4 @@
 # Use spring.profiles.active to switch between main site and indexer app, e.g.
 # --spring.profiles.active=indexer for indexer.  Site runs in default and site profiles.
 
-which java  1>&2
-ls $HOME/.jdk 1>&2
-
 java -Xmx1260m -XX:MaxPermSize=512m -classpath "." org.springframework.boot.loader.JarLauncher $*
