653f84631f57d02c1209c9dffc7970b1acadfc5d
Gonçalo Silva and Jeff Taggart <pair+gsilva+jtaggart@pivotallabs.com>
2013-08-08 18:12:33 +0100

    Committing a java location difference for CF

--- src/main/resources/main.sh
+++ src/main/resources/main.sh
@@ -3,4 +3,4 @@
 # Use spring.profiles.active to switch between main site and indexer app, e.g.
 # --spring.profiles.active=indexer for indexer.  Site runs in default and site profiles.
 
-java -Xmx1260m -XX:MaxPermSize=512m -classpath "." org.springframework.boot.loader.JarLauncher $*
+$HOME/.java/bin/java -Xmx1260m -XX:MaxPermSize=512m -classpath "." org.springframework.boot.loader.JarLauncher $*
