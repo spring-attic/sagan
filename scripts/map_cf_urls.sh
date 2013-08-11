324a281e8f42bc4ab51d97ba46a1d5136af2c7f1
Chris Beams <cbeams@gopivotal.com>
2013-08-11 13:01:51 +0200

    Add script to automatically map URLs to sagan app

---
+++ scripts/map_cf_urls.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+
+cf map sagan interface21.com &
+cf map sagan spring.io &
+cf map sagan springframework.io &
+cf map sagan springframework.org &
+cf map sagan springsource.com &
+cf map sagan springsource.org &
+
+cf map sagan blog interface21.com &
+cf map sagan blog springsource.com &
+cf map sagan blog springsource.org &
+
+cf map sagan www interface21.com &
+cf map sagan www spring.io &
+cf map sagan www springframework.io &
+cf map sagan www springframework.org &
+cf map sagan www springsource.com &
+cf map sagan www springsource.org &
