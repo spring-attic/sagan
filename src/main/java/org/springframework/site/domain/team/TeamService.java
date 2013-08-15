9cb9b615112d1baeac68c6754661cc2eefca94f8
Jeff Taggart and Nick Street <pair+jtaggart+nstreet@pivotallabs.com>
2013-08-15 10:44:08 +0100

    Fixed deprecation warning

--- src/main/java/org/springframework/site/domain/team/TeamService.java
+++ src/main/java/org/springframework/site/domain/team/TeamService.java
@@ -4,7 +4,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
-import org.springframework.security.authentication.encoding.PasswordEncoder;
 import org.springframework.site.search.SearchService;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
@@ -58,7 +57,7 @@ public class TeamService {
 		existingProfile.setGravatarEmail(profile.getGravatarEmail());
 
 		if (!StringUtils.isEmpty(profile.getGravatarEmail())) {
-			PasswordEncoder encoder = new Md5PasswordEncoder();
+			Md5PasswordEncoder encoder = new Md5PasswordEncoder();
 			String hashedEmail = encoder.encodePassword(profile.getGravatarEmail(), null);
 			existingProfile.setAvatarUrl(String.format("http://gravatar.com/avatar/%s", hashedEmail));
 		}
