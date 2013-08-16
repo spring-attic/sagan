14424f340e89f5165402f1b942344e0c1ba19714
Chris Beams <cbeams@gopivotal.com>
2013-08-16 15:08:36 +0200

    Rename remaining 'shortName' refs to 'versionDisplayName'

--- src/test/java/org/springframework/site/domain/projects/ProjectVersionTests.java
+++ src/test/java/org/springframework/site/domain/projects/ProjectVersionTests.java
@@ -8,19 +8,19 @@ import static org.hamcrest.Matchers.equalTo;
 public class ProjectVersionTests {
 
 	@Test
-	public void getShortNameForCurrentVersion() {
+	public void getDisplayNameForCurrentVersion() {
 		ProjectRelease version = new ProjectRelease("1.2.3.RELEASE", ProjectRelease.ReleaseStatus.CURRENT, "", "", "", "");
 		assertThat(version.getVersionDisplayName(), equalTo("1.2.3"));
 	}
 
 	@Test
-	public void getShortNameForPreReleaseVersion() {
+	public void getDisplayNameForPreReleaseVersion() {
 		ProjectRelease version = new ProjectRelease("1.2.3.M1", ProjectRelease.ReleaseStatus.PRERELEASE, "", "", "", "");
 		assertThat(version.getVersionDisplayName(), equalTo("1.2.3.M1"));
 	}
 
 	@Test
-	public void getShortNameForOtherVersion() {
+	public void getDisplayNameForOtherVersion() {
 		ProjectRelease version = new ProjectRelease("1.2.3.RC1", ProjectRelease.ReleaseStatus.PRERELEASE,"", "", "", "");
 		assertThat(version.getVersionDisplayName(), equalTo("1.2.3.RC1"));
 	}
