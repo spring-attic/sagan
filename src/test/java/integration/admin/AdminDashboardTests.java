e9248fe9878d6de3947bf62c5650dee33d941595
Rob Winch <rwinch@vmware.com>
2013-08-26 00:34:02 -0500

    Enable CSRF for /admin
    This adds the CSRF token to the admin forms. It also adds the
    springSecurityFilterChain to the tests.
    
    [Fixes #55787736]

--- src/test/java/integration/admin/AdminDashboardTests.java
+++ src/test/java/integration/admin/AdminDashboardTests.java
@@ -1,29 +1,15 @@
 package integration.admin;
 
 import integration.IntegrationTestBase;
-import org.junit.Before;
 import org.junit.Test;
-
-import java.security.Principal;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class AdminDashboardTests extends IntegrationTestBase {
-	private Principal principal;
-
-	@Before
-	public void setup() {
-		principal = new Principal() {
-			@Override
-			public String getName() {
-				return "admin";
-			}
-		};
-	}
 
 	@Test
 	public void adminDashboard() throws Exception {
-		mockMvc.perform(get("/admin").principal(principal)).andExpect(status().isOk());
+		mockMvc.perform(get("/admin")).andExpect(status().isOk());
 	}
 }
