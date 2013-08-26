8a9a937b2fd4fb6b08046a2ce4d4137b215f2716
David Friermor <pair+dfriermor@pivotallabs.com>
2013-08-26 11:24:35 -0700

    removed search specific additions to run.sh and app.yml

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ git pull --rebase
 mvn clean compile
 classpath=$(mvn exec:exec -Dexec.executable="echo" -Dexec.args="CLASSPATH %classpath" | grep "^CLASSPATH" | cut -d" " -f2)
 mvn sass:watch &
-mvn exec:exec -Dexec.executable="java" -Dexec.args="-classpath src/main/resources:$classpath org.springframework.site.web.configuration.ApplicationConfiguration" -Delasticsearch.client.endpoint="https://qlmsjwfa.api.qbox.io"
+mvn exec:exec -Dexec.executable="java" -Dexec.args="-classpath src/main/resources:$classpath org.springframework.site.web.configuration.ApplicationConfiguration"
