a3ed25e9e7d714dad6a0645bf69309ac3caeb9b9
Chloe Jackson <jacksonc@vmware.com>
2013-08-26 18:18:55 -0700

    Updated Marketo Newsletter Subscription Form - embedded, no iframe

--- src/main/resources/static/js/application.js
+++ src/main/resources/static/js/application.js
@@ -179,5 +179,72 @@ var detectArch = function () {
 
 
 
+/* Marketo Newsletter Subscription Form */
+
+function fieldValidate(field) {
+  return true;
+}
+var profiling = {
+  isEnabled: false,
+  numberOfProfilingFields: 3,
+  alwaysShowFields: [ 'mktDummyEntry']
+};
+var mktFormLanguage = 'English'
+function mktoGetForm() {return document.getElementById('mktForm_1035'); }
+
+
+
+/* Subscribed cookie */
+
+function getCookie(c_name)
+{
+var c_value = document.cookie;
+var c_start = c_value.indexOf(" " + c_name + "=");
+if (c_start == -1)
+  {
+  c_start = c_value.indexOf(c_name + "=");
+  }
+if (c_start == -1)
+  {
+  c_value = null;
+  }
+else
+  {
+  c_start = c_value.indexOf("=", c_start) + 1;
+  var c_end = c_value.indexOf(";", c_start);
+  if (c_end == -1)
+    {
+    c_end = c_value.length;
+    }
+  c_value = unescape(c_value.substring(c_start,c_end));
+  }
+return c_value;
+}
+
+function setCookie(c_name,value,exdays)
+{
+  var exdate=new Date();
+  exdate.setDate(exdate.getDate() + exdays);
+  var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+  document.cookie=c_name + "=" + c_value;
+}
+
+var subscribed=getCookie("subscribed");
+
+function setSubscribeCookie() 
+{
+  setCookie("subscribed",subscribed,365);
+}
+
+function checkCookie()
+{
+if (subscribed)
+  {
+  document.getElementsByName('Email')[0].placeholder='Subscribed';
+  }
+}
+
+
+
 
 
