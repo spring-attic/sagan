b7cf72d297a726914e9507cade380eba8888754b
Dave Syer <dsyer@gopivotal.com>
2013-08-29 09:47:31 +0100

    Fix performance deployment

--- deploy-simple.sh
+++ deploy-simple.sh
@@ -19,8 +19,6 @@ SPACE=$4
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 
-PATH=$PATH:~/.rvm/rubies/ruby-1.9.3-p429/bin/:~/.rvm/bin/
-
 rvm use 1.9.3@sagan-ops
 
 $CF target api.run.pivotal.io || exit
