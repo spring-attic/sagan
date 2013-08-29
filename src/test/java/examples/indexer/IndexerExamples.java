2b1d69a01fd74ca44adb20cdac49c813aa755872
Dave Syer <dsyer@gopivotal.com>
2013-08-29 08:59:20 +0100

    Add LongRunning @Rule

--- src/test/java/examples/indexer/IndexerExamples.java
+++ src/test/java/examples/indexer/IndexerExamples.java
@@ -1,7 +1,5 @@
 package examples.indexer;
 
-
-import com.google.common.collect.Iterables;
 import integration.configuration.InMemoryElasticSearchConfiguration;
 import integration.search.SearchIndexSetup;
 import io.searchbox.client.JestClient;
@@ -13,6 +11,9 @@ import io.spring.site.indexer.configuration.IndexerConfiguration;
 import io.spring.site.search.SearchResult;
 import io.spring.site.search.SearchResults;
 import io.spring.site.search.SearchService;
+
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.ClassRule;
@@ -27,9 +28,12 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import utils.LongRunning;
 import utils.SetSystemProperty;
 
-import java.util.List;
+import com.google.common.collect.Iterables;
+
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThan;
@@ -37,15 +41,19 @@ import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@ContextConfiguration(classes = {IndexerConfiguration.class,
-								 InMemoryElasticSearchConfiguration.class },
-					  initializers = {ConfigFileApplicationContextInitializer.class,
-									  LoggingApplicationContextInitializer.class })
+@ContextConfiguration(classes = { IndexerConfiguration.class,
+		InMemoryElasticSearchConfiguration.class }, initializers = {
+		ConfigFileApplicationContextInitializer.class,
+		LoggingApplicationContextInitializer.class })
 @DirtiesContext
 public class IndexerExamples {
 
 	@ClassRule
-	public static SetSystemProperty delay = new SetSystemProperty("search.indexer.delay", "60000000");
+	public static LongRunning longRunning = LongRunning.create();
+
+	@ClassRule
+	public static SetSystemProperty delay = new SetSystemProperty("search.indexer.delay",
+			"60000000");
 
 	@Autowired
 	private GettingStartedGuideIndexer gettingStartedGuideIndexer;
@@ -74,37 +82,43 @@ public class IndexerExamples {
 
 	@Test
 	public void gettingStartedGuideIndexer_indexableItems() {
-		Iterable<Guide> items = gettingStartedGuideIndexer.indexableItems();
+		Iterable<Guide> items = this.gettingStartedGuideIndexer.indexableItems();
 		assertThat(Iterables.size(items), greaterThan(0));
 	}
 
 	@Test
 	public void gettingStartedGuideIndexer_indexesAnItem() {
-		Guide gettingStartedGuide = new Guide("gs-rest-service", "rest-service", "title", "subtitle", "rest", "This is the sidebar!");
-		gettingStartedGuideIndexer.indexItem(gettingStartedGuide);
+		Guide gettingStartedGuide = new Guide("gs-rest-service", "rest-service", "title",
+				"subtitle", "rest", "This is the sidebar!");
+		this.gettingStartedGuideIndexer.indexItem(gettingStartedGuide);
 
-		SearchResults searchResults = searchService.search("rest", new PageRequest(0, 10), null);
+		SearchResults searchResults = this.searchService.search("rest", new PageRequest(
+				0, 10), null);
 		assertThat(searchResults.getPage().getContent().size(), is(1));
 	}
 
 	@Test
 	public void understandingGuideIndexer_indexableItems() {
-		Iterable<UnderstandingGuide> items = understandingGuideIndexer.indexableItems();
+		Iterable<UnderstandingGuide> items = this.understandingGuideIndexer
+				.indexableItems();
 		assertThat(Iterables.size(items), greaterThan(8));
 	}
 
 	@Test
 	public void understandingGuideIndexer_indexesAnItem() throws Exception {
-		UnderstandingGuide guide = new UnderstandingGuide("foo", "<h1>Understanding: foo</h1><p>content</p>", "<p>sidebar</p>");
-		understandingGuideIndexer.indexItem(guide);
+		UnderstandingGuide guide = new UnderstandingGuide("foo",
+				"<h1>Understanding: foo</h1><p>content</p>", "<p>sidebar</p>");
+		this.understandingGuideIndexer.indexItem(guide);
 
-		SearchResults searchResults = searchService.search("foo", new PageRequest(0, 10), null);
+		SearchResults searchResults = this.searchService.search("foo", new PageRequest(0,
+				10), null);
 		List<SearchResult> results = searchResults.getPage().getContent();
 		assertThat(results.size(), is(1));
 	}
 
 	private void rebuildIndex() {
-		SearchIndexSetup searchIndexSetup = new SearchIndexSetup(jestClient, index);
+		SearchIndexSetup searchIndexSetup = new SearchIndexSetup(this.jestClient,
+				this.index);
 		searchIndexSetup.deleteIndex();
 		searchIndexSetup.createIndex();
 	}
