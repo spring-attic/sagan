26da87a3b7ddd0873c145b71f15ea86953d53361
Gonçalo Silva <gsilva@pivotallabs.com>
2013-09-10 15:46:08 +0100

    Improving health status detection in the polling script
    - checking for status of 'running' or a value above 0%
    - failing the script if the status is 'staging failed'

--- scripts/wait-for-app-to-start.sh
+++ scripts/wait-for-app-to-start.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-if [ $# != 2 ] ; then cat << EOM
-
+if [ $# != 3 ] ; then cat << EOM
     This script polls 'cf health' repeteadly to find out if an app is running.
 
     It will stop when an app is running or if a maximun number of retries is reached.
@@ -10,7 +9,6 @@ if [ $# != 2 ] ; then cat << EOM
 EOM
     exit
 fi
-
 
 APP_NAME=$1
 MAX_RETRIES=$2
@@ -21,11 +19,21 @@ echo "Polling cf health for $APP_NAME"
 RETRY=0
 while [ $RETRY -lt $MAX_RETRIES ]; do
     HEALTH=`$CF health $APP_NAME`
-    if [[ $HEALTH != "stopped" && $HEALTH != "0%" ]]; then
+
+    if [[ $HEALTH =~ running|[1-9][0-9]?% ]]; then
         echo "$APP_NAME is running!"
         exit 0
     fi
+
+    if [[ $HEALTH == "staging failed" ]]; then
+        echo "$APP_NAME failed to stage. Needs to be re-deployed"
+        exit 1
+    fi
+
     let RETRY=RETRY+1
+
     echo "retry $RETRY of $MAX_RETRIES where '$APP_NAME' health is '$HEALTH'"
 done
+
+echo "Polling of $APP_NAME reached the maximum number of retries"
 exit 1
