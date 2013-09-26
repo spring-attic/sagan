949fe2525bacb607314094751b3e24f16fceb089
Chris Beams <cbeams@gopivotal.com>
2013-09-26 18:18:05 +0200

    Simplify run.sh script
    With SASS out of the picture, run.sh can now be as simple as
    
        mvn clean spring-boot:run
    
    [#57712296]

--- run.sh
+++ run.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-mvn clean compile
-classpath=$(mvn exec:exec -Dexec.executable="echo" -Dexec.args="CLASSPATH %classpath" | grep "^CLASSPATH" | cut -d" " -f2)
-mvn sass:watch &
-mvn exec:exec -Dexec.executable="java" -Dexec.args="-classpath src/main/resources:$classpath io.spring.site.web.configuration.ApplicationConfiguration"
+mvn clean spring-boot:run
