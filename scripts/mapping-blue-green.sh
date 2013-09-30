bc9bb3ba22506722674a4bace63640795ebe7152
Chris Beams <cbeams@gopivotal.com>
2013-09-30 15:31:50 +0200

    Remove legacy domain mapping
    Legacy domain redirects are now handled by the dedicated redirect app.

--- scripts/mapping-blue-green.sh
+++ scripts/mapping-blue-green.sh
@@ -29,32 +29,13 @@ $CF space $SPACE || exit
 
 if [ $SPACE == "staging" ]; then
 
-    ($CF map $NEXT staging spring.io && $CF unmap staging.spring.io $CURRENT)
+    $CF map $NEXT staging spring.io && $CF unmap staging.spring.io $CURRENT
 
 elif [ $SPACE == "production" ]; then
 
     ($CF map $NEXT spring.io && $CF unmap .spring.io $CURRENT) &
-    ($CF map $NEXT static springsource.org && $CF unmap static.springsource.org $CURRENT) &
-    ($CF map $NEXT www springsource.org && $CF unmap www.springsource.org $CURRENT) &
-    ($CF map $NEXT forum springsource.org && $CF unmap forum.springsource.org $CURRENT) &
-    ($CF map $NEXT blog springsource.org && $CF unmap blog.springsource.org $CURRENT) &
-    ($CF map $NEXT springsource.org && $CF unmap .springsource.org $CURRENT) &
-    ($CF map $NEXT springframework.org && $CF unmap .springframework.org $CURRENT) &
-    ($CF map $NEXT www springframework.org && $CF unmap www.springframework.org $CURRENT) &
-    ($CF map $NEXT static springframework.org && $CF unmap static.springframework.org $CURRENT) &
-    ($CF map $NEXT blog springsource.com && $CF unmap blog.springsource.com $CURRENT) &
-    ($CF map $NEXT springsource.com && $CF unmap .springsource.com $CURRENT) &
-    ($CF map $NEXT forum springframework.org && $CF unmap forum.springframework.org $CURRENT) &
-    ($CF map $NEXT blog interface21.com && $CF unmap blog.interface21.com $CURRENT) &
-    ($CF map $NEXT www springsource.com && $CF unmap www.springsource.com $CURRENT) &
-    ($CF map $NEXT interface21.com && $CF unmap .interface21.com $CURRENT) &
-    ($CF map $NEXT www interface21.com && $CF unmap www.interface21.com $CURRENT) &
     ($CF map $NEXT www spring.io && $CF unmap www.spring.io $CURRENT) &
-    ($CF map $NEXT springframework.io && $CF unmap .springframework.io $CURRENT) &
-    ($CF map $NEXT springsource.io && $CF unmap .springsource.io $CURRENT) &
-    ($CF map $NEXT www springframework.io && $CF unmap www.springframework.io $CURRENT) &
-    ($CF map $NEXT www springsource.io && $CF unmap www.springsource.io $CURRENT) &
 
     wait
-fi;
+fi
 
