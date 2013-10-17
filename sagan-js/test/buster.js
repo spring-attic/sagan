01d9729b686cf4cf32c2d40e7d6f00a0ee8c5c0c
Brian Cavalier <bcavalier@gopivotal.com>
2013-10-17 15:14:46 -0400

    Add basic buster selenium webdriverjs setup
    This adds the webdriverjs client wrapped in a custom buster extension
    that injects it into scenario tests (but not unit tests), and adds new
    npm scripts for running against phantomjs for now.
    
    There is apparently a bug in buster whereby exposing the
    describe-style globals before extensions are initialized causes the
    `this` context of tests to get messed up.  Consequently, I had to
    switch to exposing them in the individual test files.  We need to
    find a better way until the buster problem is fixed.

--- sagan-js/test/buster.js
+++ sagan-js/test/buster.js
@@ -3,11 +3,53 @@
  * functional scenario tests.
  */
 
-require('./buster-spec-expose');
-
 // Basic unit test configuration for tests that can run
 // in node (i.e. no DOM requirements)
 exports.unit = {
-	environment: 'node',
-	tests: ['unit/*-spec.js', 'unit/**/*-spec.js']
+  environment: 'node',
+  tests: ['unit/*-spec.js', 'unit/**/*-spec.js']
 };
+
+// Shared config for all functional scenario tests
+exports['scenario-base'] = {
+  environment: 'node',
+  tests: ['scenario/*-spec.js', 'scenario/**/*-spec.js']
+};
+
+// For now, a simple selenium setup that will use phantomjs
+exports['scenario-phantom'] = {
+  extends: 'scenario-base',
+  extensions: [require('./buster-webdriverjs')],
+  'buster-webdriverjs': {
+    driver: 'webdriverjs',
+    config: {
+      desiredCapabilities: {
+        browserName: 'phantomjs'
+      },
+      logLevel: 'silent'
+    }
+  }
+};
+
+// TODO: Enable and configure once selenium server is setup
+//exports['scenario'] = {
+//  extends: 'scenario-base',
+//  extensions: [require('./buster-webdriverjs')],
+//  'buster-webdriverjs': {
+//    driver: 'webdriverjs',
+//    config: {
+//      // TODO: Change these
+//      desiredCapabilities: {
+//        browserName: 'safari',
+//        version: '6',
+//        platform: 'Mac 10.8'
+//      },
+//      host: '<selenium server hostname here>',
+//      port: 80,
+//      // If needed, use environment vars
+//      user: process.env.WHATEVER,
+//      key: process.env.WHATEVER,
+//      logLevel: 'silent'
+//    }
+//  }
+//};
