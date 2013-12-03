abdb57fcf2e8c16057974e2fab7b9d714320eb22
Brian Cavalier <bcavalier@gopivotal.com>
2013-12-03 15:59:28 -0500

    Add selenium tests for docs page

---
+++ sagan-js/test/lib/wd/getAttributeValueOfVisible.js
@@ -0,0 +1,23 @@
+var when = require('when');
+
+/**
+ * Curried function that returns a promise for an array of
+ * attribute values of all *visible* elements
+ */
+module.exports = function getAttributeValueOfVisible(attributeName) {
+    return function (elements) {
+        // Essentially simulating array.filter, but because everything
+        // is async, we lean on when.reduce for the heavy lifting.
+        // This reduce produces a smaller array of promises.
+        // Use when.all at the end to ensure each promise in the array
+        // has resolved.
+        return when.reduce(elements,function (visible, element) {
+            return element.isVisible().then(function (isVisible) {
+                if (isVisible) {
+                    visible.push(element.getAttribute(attributeName));
+                }
+                return visible;
+            });
+        }, []).then(when.all);
+    };
+}
