c7be15b3ccaad996f53e13f26d15cdb2b09cae25
Brian Cavalier <bcavalier@gopivotal.com>
2013-12-03 15:59:28 -0500

    Add selenium tests for docs page

--- sagan-js/test/scenario/docs-spec.js
+++ sagan-js/test/scenario/docs-spec.js
@@ -1,5 +1,5 @@
 /**
- * guides page functional tests
+ * docs page functional tests
  */
 
 // For advanced promise combinators like map, reduce.
@@ -7,11 +7,13 @@ var getAttributeValueOfVisible = require('../lib/wd/getAttributeValueOfVisible')
 
 var filterInputId = 'doc_filter';
 var filterAttribute = 'data-filterable';
+var infoPopupTrigger = '.js-item--open-dropdown';
+var infoPopup = '.item--dropdown';
 
-describe('Guides page', function () {
+describe('Docs page', function () {
 
     before(function () {
-        return this.browser.path('/guides');
+        return this.browser.path('/docs');
     });
 
     describe('filter field', function () {
@@ -20,18 +22,18 @@ describe('Guides page', function () {
             // Once the autofocus is fixed, change itEventually to it
             return this.browser
                 .setImplicitWaitTimeout(5000)
-                    .active()
-                    .getAttribute('id').then(function(id) {
-                        expect(id).toBe('doc_filter');
-                    });
+                .active()
+                .getAttribute('id').then(function(id) {
+                    expect(id).toBe('doc_filter');
+                });
         });
 
         it('should filter results when filter text matches', function () {
-            var filterText = 'upload';
+            var filterText = 'amqp';
 
             return this.browser
                 .elementById(filterInputId)
-                    .type(filterText)
+                .type(filterText)
                 .elementsByCssSelector('[' + filterAttribute + ']')
                     .then(getAttributeValueOfVisible(filterAttribute))
                     .then(function (attributeValues) {
@@ -39,7 +41,6 @@ describe('Guides page', function () {
                             expect(value.toLowerCase()).toContain(filterText);
                         });
                     });
-
         });
 
         it('should show no results when filter text doesn\'t match', function () {
@@ -54,6 +55,28 @@ describe('Guides page', function () {
                         expect(attributeValues.length).toBe(0);
                     });
 
+        });
+    });
+
+    describe('info popup', function() {
+
+        it('should appear when doc item is clicked and then disappear when clicked again', function() {
+        	return this.browser
+                .setImplicitWaitTimeout(5000)
+                .elementByCssSelector(infoPopupTrigger)
+                    .click()
+                .elementByCssSelector(infoPopup)
+                    .isVisible()
+                    .then(function(visible) {
+                        expect(visible).toBeTrue();
+                    })
+                .elementByCssSelector(infoPopupTrigger)
+                    .click()
+                .elementByCssSelector(infoPopup)
+                    .isVisible()
+                    .then(function(visible) {
+                        expect(visible).toBeFalse();
+                    });
         });
     });
 
