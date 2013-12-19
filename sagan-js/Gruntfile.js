249180a82f902dd4618059a842d5b82ec0612dcb
Brian Cavalier <bcavalier@gopivotal.com>
2013-12-19 16:39:33 -0500

    Start integrating css build via grunt and gradle build

---
+++ sagan-js/Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        cssmin: {
+            combine: {
+                files: {
+                    '../sagan-site/build/resources/static/css/main.css': '../sagan-site/src/main/resources/static/css/main.css'
+//                    'dist/css/main.css': '../sagan-site/src/main/resources/static/css/main.css'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.registerTask('default', ['cssmin']);
+};
