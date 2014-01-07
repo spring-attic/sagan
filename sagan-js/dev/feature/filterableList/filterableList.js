5896c64fe83ce1c1d1422372a8b6d252eb9322d2
John Hann <jhann@gopivotal.com>
2014-01-07 16:08:59 -0500

    Update filterableList feature so that it uses the filter=[text] query parameter in the document url, if supplied.
    * Change filterableList module to take a value instead of an event.
    * Create getUrlFilter module to scrape filter query parameter from document url.
    * Add "input" event to catch context-menu (e.g. right-click) events, not just keyboard events.
    * Update filterableList/main with new behavior.

--- sagan-js/dev/feature/filterableList/filterableList.js
+++ sagan-js/dev/feature/filterableList/filterableList.js
@@ -2,7 +2,7 @@ var filter = require('./filter');
 var createAttributeMatcher = require('./attributeMatcher');
 var $ = require('jquery');
 
-module.exports = function createFilterableList(onMatch, filterAttribute, containers) {
+module.exports = function filterableList(onMatch, filterAttribute, containers) {
     var filterMatches = filter.create(
         onMatch,
         containers.map(function (node) {
@@ -15,7 +15,7 @@ module.exports = function createFilterableList(onMatch, filterAttribute, contain
 
     var attributeMatcher = createAttributeMatcher(filterAttribute);
 
-    return function filterTargetValue(e) {
-        return filterMatches(attributeMatcher(e.target.value));
+    return function filterByValue(value) {
+        return filterMatches(attributeMatcher(value));
     };
 };
--- sagan-js/dev/feature/filterableList/main.js
+++ sagan-js/dev/feature/filterableList/main.js
@@ -1,12 +1,11 @@
 var filterableList = require('./filterableList');
+var getUrlFilter = require('./getUrlFilter');
 var $ = require('jquery');
 
-module.exports = function initFilterableList() {
-    return createFilterableList(document.getElementById('doc_filter'));
-};
+module.exports = createFilterableList;
 
-function createFilterableList(filterInput) {
-    var filterList;
+function createFilterableList() {
+    var filterList, filterInput;
 
     $(ready);
 
@@ -16,18 +15,31 @@ function createFilterableList(filterInput) {
 
     function ready() {
         var containers = $('[data-filterable-container]').get();
+        var initialFilter = getUrlFilter();
+
+        filterInput = document.getElementById('doc_filter');
         filterList = filterableList(onFilterMatch, 'data-filterable', containers);
 
-        $(filterInput).on('keyup', filterList);
+        if (initialFilter) {
+            $(filterInput).val(initialFilter);
+            filterList(initialFilter);
+        }
+        // jquery has a "paste" event, but it fires too late.
+        // "keyup" and "input" should catch mostly everything.
+        $(filterInput).on('keyup input', onInputChange);
 
         function onFilterMatch(matched, node) {
             $(node).toggleClass('filterable-non-matching', !matched);
         }
     }
 
+    function onInputChange (e) {
+        filterList(e.target.value);
+    }
+
     function destroy() {
         if(filterList) {
-            $(filterInput).off('keyup', filterList);
+            $(filterInput).off('keyup input', filterList);
         }
     }
 }
