09b9ec1714e4067e3a470e5760313fa7f555518c
Chris Beams <cbeams@gopivotal.com>
2014-02-25 11:57:04 +0100

    Update bisect script to run full build

--- util/bisect.sh
+++ util/bisect.sh
@@ -2,5 +2,5 @@
 BAD=962958c
 GOOD=b742859
 git bisect start $BAD $GOOD --
-git bisect run ./gradlew -x gitProperties clean :sagan-indexer:integTest
+git bisect run ./gradlew clean build
 git bisect reset
