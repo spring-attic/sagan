1b9490cb7aefdb702cb2540cf39c8ac7a3dc1329
Chris Beams <cbeams@gopivotal.com>
2014-03-12 14:20:04 +0100

    Refactor StubGithubRestClient

--- sagan-site/src/it/java/integration/IntegrationTestsConfig.java
+++ sagan-site/src/it/java/integration/IntegrationTestsConfig.java
@@ -3,17 +3,17 @@ package integration;
 import sagan.SiteConfig;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestTemplate;
 
+import sagan.support.github.StubGithubRestClient;
+
 import static org.mockito.Mockito.mock;
 
 @Configuration
 @Import(SiteConfig.class)
-@ComponentScan("integration.stubs")
 class IntegrationTestsConfig {
 
     @Primary
@@ -22,4 +22,9 @@ class IntegrationTestsConfig {
         return mock(RestTemplate.class);
     }
 
+    @Primary
+    @Bean
+    public StubGithubRestClient stubGithubRestClient() {
+        return new StubGithubRestClient();
+    }
 }
