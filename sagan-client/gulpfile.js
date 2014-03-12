a33df29202acc2b136d70f59eb236cb59b10d4d1
Brian Clozel <bclozel@gopivotal.com>
2014-03-12 23:16:41 +0100

    Update gulp build
    * use gulp-cram
    * integrate bower as a prebublish step and don't use gulp-bower
    * upgrade to bwoer 1.3.1

--- sagan-client/gulpfile.js
+++ sagan-client/gulpfile.js
@@ -1,8 +1,7 @@
-var combine = require('gulp-util').combine,
-    gulpFilter = require('gulp-filter'),
+var gulpFilter = require('gulp-filter'),
+    cram = require('gulp-cram'),
     uglify = require('gulp-uglify'),
     imagemin = require('gulp-imagemin'),
-    bower = require('gulp-bower'),
     bowerSrc = require('gulp-bower-src'),
     cssmin = require('gulp-minify-css'),
     watch = require('gulp-watch'),
@@ -11,7 +10,7 @@ var combine = require('gulp-util').combine,
     es = require('event-stream');
 
 var paths = {
-    scripts: ['build/tmp/static/run.js'],
+    run: 'src/run.js',
     css: {
         files: ['src/css/main.css', 'src/css/guide.css'],
         root: 'src/css'
@@ -22,11 +21,6 @@ var paths = {
 };
 
 
-// install bower dependencies listed in "bower.json" in ./src/lib
-gulp.task('bower-install', function(){
-    return bower().pipe(gulp.dest('./src/lib'));
-});
-
 // concat and minify CSS files
 gulp.task('minify-css', function() {
     return gulp.src(paths.css.files)
@@ -34,9 +28,15 @@ gulp.task('minify-css', function() {
         .pipe(gulp.dest(paths.dest+'css'));
 });
 
-// uglify JavaScript source files
-gulp.task('minify-scripts', function() {
-    return gulp.src(paths.scripts)
+// cram and uglify JavaScript source files
+gulp.task('build-scripts', function() {
+
+    var opts = {
+        includes: [ 'curl/loader/legacy', 'curl/loader/cjsm11'],
+        excludes: ['leaflet']
+    };
+
+    return cram(paths.run, opts).into('run.js')
         .pipe(uglify())
         .pipe(gulp.dest(paths.dest));
 });
@@ -64,9 +64,7 @@ gulp.task('copy-assets', function() {
         .pipe(gulp.dest(paths.dest));
 })
 
-gulp.task('default', ['bower-install'], function(){ });
-
-gulp.task('build', ['bower-install', 'minify-css', 'minify-scripts', 'optim-img', 'copy-assets', 'bower-files'], function(){ });
+gulp.task('build', ['minify-css', 'build-scripts', 'optim-img', 'copy-assets', 'bower-files'], function(){ });
 
 // temp tasks, until live reloading is in configured
 gulp.task('bower-gradle', function() {
@@ -74,7 +72,7 @@ gulp.task('bower-gradle', function() {
         .pipe(gulp.dest('../sagan-site/build/resources/main/static/'+'lib'));
 })
 
-gulp.task('watch', ['bower-install', 'bower-gradle'], function(){
+gulp.task('watch', ['bower-gradle'], function(){
     return gulp.src(['src/**', '!src/lib/**'])
         .pipe(watch())
         .pipe(gulp.dest('../sagan-site/build/resources/main/static/'))
