4822b1a490d233f1b8a5492ba0770856649010aa
Chris Beams <cbeams@gopivotal.com>
2014-05-12 15:04:56 +0200

    Seed stackoverflow tag data

--- sagan-common/src/main/resources/database/V4__stackoverflow_tags.sql
+++ sagan-common/src/main/resources/database/V4__stackoverflow_tags.sql
@@ -1,1 +1,7 @@
 alter table project add stackoverflowtags character varying(255);
+
+update project set stackoverflowtags = 'spring,spring-core,spring-framework,dependency-injection' where id = 'spring-framework';
+update project set stackoverflowtags = 'spring-security' where id = 'spring-security';
+update project set stackoverflowtags = 'spring-data,spring-data-mongodb,spring-data-neo4j' where id = 'spring-data';
+update project set stackoverflowtags = 'spring-social,spring-social-facebook' where id = 'spring-social';
+update project set stackoverflowtags = 'spring-batch,jsr-352' where id = 'spring-batch';
