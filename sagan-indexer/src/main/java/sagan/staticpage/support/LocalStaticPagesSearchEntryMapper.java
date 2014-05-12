0fdf94696587b555dac50731b28278c33c49779d
Antti Markus <antti.markus+github@gmail.com>
2014-05-12 21:47:23 +0300

    Migrate LocalStaticPagesSearchEntryMapper

--- sagan-indexer/src/main/java/sagan/staticpage/support/LocalStaticPagesSearchEntryMapper.java
+++ sagan-indexer/src/main/java/sagan/staticpage/support/LocalStaticPagesSearchEntryMapper.java
@@ -2,9 +2,9 @@ package sagan.staticpage.support;
 
 import sagan.search.SearchEntry;
 import sagan.search.SearchEntryMapper;
-import sagan.support.DateConverter;
 
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 import org.jsoup.nodes.Document;
 
@@ -13,7 +13,7 @@ public class LocalStaticPagesSearchEntryMapper implements SearchEntryMapper<Docu
     @Override
     public SearchEntry map(Document document) {
         SearchEntry entry = new SearchEntry();
-        entry.setPublishAt(DateConverter.toLocalDateTime(new Date(0L)));
+        entry.setPublishAt(LocalDateTime.ofEpochSecond(0, 0, ZoneOffset.UTC));
         String text = document.getElementsByClass("body--container").text();
         entry.setRawContent(text);
         entry.setSummary(text.substring(0, Math.min(500, text.length())));
