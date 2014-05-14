094243cff97b8660813579346eaca9b43683b6e8
Chris Beams <cbeams@gopivotal.com>
2014-05-14 18:10:34 +0200

    Simplify QuestionsController

--- sagan-site/src/main/java/sagan/questions/support/QuestionsController.java
+++ sagan-site/src/main/java/sagan/questions/support/QuestionsController.java
@@ -1,9 +1,7 @@
 package sagan.questions.support;
 
-import sagan.projects.Project;
 import sagan.projects.support.ProjectMetadataService;
 
-import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,15 +22,12 @@ class QuestionsController {
     @RequestMapping("/questions")
     public String show(Model model) {
 
-        model.addAttribute("projects", projectsWithStackOverflowTags());
-        return "questions/index";
-    }
-
-    private List<Project> projectsWithStackOverflowTags() {
-        return service.getProjects().stream()
+        model.addAttribute("projects", service.getProjects().stream()
                 .filter(project -> !project.getCategory().equals("attic"))
                 .filter(project -> !project.getStackOverflowTagList().isEmpty())
                 .sorted((p1, p2) -> p1.getName().compareToIgnoreCase(p2.getName()))
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()));
+        return "questions/index";
     }
+
 }
