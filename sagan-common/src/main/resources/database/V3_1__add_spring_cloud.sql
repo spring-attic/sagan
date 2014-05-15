b25aa8a0f5349f30ed6c785a5f193a4795b02816
Brian Clozel <bclozel@gopivotal.com>
2014-05-15 23:30:38 +0200

    Add db migration file for Spring Cloud

---
+++ sagan-common/src/main/resources/database/V3_1__add_spring_cloud.sql
@@ -0,0 +1,4 @@
+insert into project values('spring-cloud','Spring Cloud','http://github.com/spring-projects/spring-cloud','incubator','http://projects.spring.io/spring-cloud','FALSE');
+insert into project_releaselist values('spring-cloud','spring-milestones','http://docs.spring.io/spring-cloud/docs/1.0.0.RC1/api/','spring-cloud','org.springframework.cloud','TRUE','https://github.com/spring-projects/spring-cloud/blob/master/README.md','1','1.0.0.RC1');
+
+
