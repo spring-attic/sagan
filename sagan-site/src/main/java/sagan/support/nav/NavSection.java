eb4f60f116fbd04030d8082728f48e79ed4ded35
Chris Beams <cbeams@gopivotal.com>
2014-05-27 14:34:06 +0200

    Add class-level Javadoc for sagan.projects.nav.*

--- sagan-site/src/main/java/sagan/support/nav/NavSection.java
+++ sagan-site/src/main/java/sagan/support/nav/NavSection.java
@@ -1,8 +1,17 @@
 package sagan.support.nav;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+/**
+ * Annotation to be used at the class level on Controllers to indicate which section in
+ * the site's main navigation should be highlighted.
+ *
+ * @see sagan.MvcConfig#addInterceptors(org.springframework.web.servlet.config.annotation.InterceptorRegistry)
+ */
+@Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface NavSection {
 
