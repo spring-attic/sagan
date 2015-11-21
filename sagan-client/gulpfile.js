e207b59086ff90fe5f3bf1cf7be2d84509bee28c
Jean de Klerk <jadekler@gmail.com>
2015-11-20 22:15:03 -0700

    Replaced bower with jspm/system.js
    - Remove bower
    - Move clipboard buttons html directly into js
    - Removed bower and various js compilation from gulp
    - Added external css copy job to gulp
    - Removed run.js (to be re-added possibly?), pointed directly to main.js+config.js
    - Several updates to dependencies in the switch from bower registry to jspm/npm/github
      - Some version re-setting still needs to be done here
      - Some bug-searching/fixing still needs to be done here
      - General QA/improvement needed here (esp clipboardButtons.js jquery hack)
    - Resource path set from /sagan-client/src/ to /sagan-client/ to include config.js, main.js, etc.
    - Updated readme by removing bower instructions
      - Readme needs serious re-updating once the dust settles

--- sagan-client/gulpfile.js
+++ sagan-client/gulpfile.js
@@ -1,60 +1,44 @@
-var gulpFilter = require('gulp-filter'),
-    cram = require('gulp-cram'),
-    uglify = require('gulp-uglify'),
-    bowerSrc = require('gulp-bower-src'),
-    sourcemaps = require('gulp-sourcemaps'),
-    cssmin = require('gulp-minify-css'),
-    gulp = require('gulp');
+var cssmin = require('gulp-minify-css'),
+  gulp = require('gulp');
 
 var paths = {
-    run: 'src/run.js',
-    css: {
-        files: ['src/css/*.css'],
-        root: 'src/css'
-    },
-    assets: ['src/img*/**','src/*.txt','src/*.html','src/font*/**','src/css*/filterable-list.css'],
-    dest: './dist/'
+  css: {
+    files: ['src/css/*.css'],
+    root: 'src/css'
+  },
+  external_css: [
+    'jspm_packages/npm/eonasdan-bootstrap-datetimepicker@4.15.35/build/css/bootstrap-datetimepicker.min.css',
+    'node_modules/twitter-bootstrap/docs/assets/css/bootstrap.css',
+    'node_modules/font-awesome/css/font-awesome.min.css'
+  ],
+  assets: ['src/img*/**', 'src/*.txt', 'src/*.html', 'src/font*/**', 'src/css*/filterable-list.css'],
+  js: ['config.js', 'build.js', 'build.js.map', 'jspm_packages/system.js', 'src/**/*'],
+  dest: './dist/'
 };
-
 
 // concat and minify CSS files
 gulp.task('minify-css', function() {
-    return gulp.src(paths.css.files)
-        .pipe(cssmin({root:paths.css.root}))
-        .pipe(gulp.dest(paths.dest+'css'));
+  return gulp.src(paths.css.files)
+    .pipe(cssmin({root: paths.css.root}))
+    .pipe(gulp.dest(paths.dest + 'css'));
 });
 
-// cram and uglify JavaScript source files
-gulp.task('build-modules', function() {
-
-    var opts = {
-        includes: [ 'curl/loader/legacy', 'curl/loader/cjsm11'],
-        excludes: ['gmaps']
-    };
-
-    return cram(paths.run, opts).into('run.js')
-        .pipe(sourcemaps.init())
-        .pipe(uglify())
-        .pipe(sourcemaps.write("./"))
-        .pipe(gulp.dest(paths.dest));
+// copy external css
+gulp.task('copy-external-css', function() {
+  return gulp.src(paths.external_css)
+    .pipe(gulp.dest(paths.dest + 'lib/css'));
 });
-
-// copy main bower files (see bower.json) and optimize js
-gulp.task('bower-files', function() {
-    var filter = gulpFilter(["**/*.js", "!**/*.min.js"]);
-    return bowerSrc()
-        .pipe(sourcemaps.init())
-        .pipe(filter)
-        .pipe(uglify())
-        .pipe(filter.restore())
-        .pipe(sourcemaps.write("./"))
-        .pipe(gulp.dest(paths.dest+'lib'));
-})
 
 // copy assets
 gulp.task('copy-assets', function() {
-    return gulp.src(paths.assets)
-        .pipe(gulp.dest(paths.dest));
-})
+  return gulp.src(paths.assets)
+    .pipe(gulp.dest(paths.dest));
+});
 
-gulp.task('build', ['minify-css', 'build-modules', 'copy-assets', 'bower-files'], function(){ });
+// copy javascript
+gulp.task('copy-javascript', function() {
+  return gulp.src(paths.js)
+    .pipe(gulp.dest(paths.dest + 'lib'));
+});
+
+gulp.task('build', ['minify-css', 'copy-external-css', 'copy-assets', 'copy-javascript'], function() { });
