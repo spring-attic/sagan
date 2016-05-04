87ee26fd6b4619eb9f1965bcfc7015b65e70a685
Mark Douglass and Mark Kollasch <pair+mdouglass+mkollasch@pivotal.io>
2016-05-04 15:14:51 -0600

    Add link to current filter on /guides and /docs pages. Fixes #64

--- sagan-client/src/feature/filterableList/main.js
+++ sagan-client/src/feature/filterableList/main.js
@@ -5,7 +5,7 @@ var $ = require('jquery');
 module.exports = createFilterableList;
 
 function createFilterableList() {
-    var filterList, filterInput;
+    var filterList, filterInput, setFilterLink;
 
     $(ready);
 
@@ -20,10 +20,31 @@ function createFilterableList() {
         filterInput = document.getElementById('doc_filter');
         filterList = filterableList(onFilterMatch, 'data-filterable', containers);
 
+        setFilterLink = function (filter) {
+            var filterLink = $("#filter-link");
+            if (!filter) {
+                filterLink.hide();
+            } else {
+                filterLink.show();
+            }
+
+            var param = jQuery.param({filter: filter});
+            var currentSearch = window.location.search;
+            var destination = '';
+            if (!currentSearch) {
+                destination = window.location.href + '?' + param;
+            } else {
+                destination = window.location.href.replace(currentSearch, '?' + param);
+            }
+
+            filterLink.attr("href", destination);
+        };
+
         if (initialFilter) {
             $(filterInput).val(initialFilter);
             filterList(initialFilter);
         }
+        setFilterLink(initialFilter);
         // jquery has a "paste" event, but it fires too late.
         // "keyup" and "input" should catch mostly everything.
         $(filterInput).on('keyup input', onInputChange);
@@ -33,12 +54,13 @@ function createFilterableList() {
         }
     }
 
-    function onInputChange (e) {
+    function onInputChange(e) {
         filterList(e.target.value);
+        setFilterLink(e.target.value);
     }
 
     function destroy() {
-        if(filterList) {
+        if (filterList) {
             $(filterInput).off('keyup input', filterList);
         }
     }
