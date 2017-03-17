1e1de91239e37277c7425eb4101c63b92ad9f96e
Dave Syer <dsyer@pivotal.io>
2017-03-17 17:20:24 +0000

    Add API endpoints for /project_metadata
    User can GET or delete a release via /{projectId}/{version} or
    add/edit a release by POSTing the whole release to /{projectId}.
    
    Authentication is via HTTP Basic with a Github API token (as the
    username and an empty password, just like Github native APIs).
    All the new endpoints are protected.

--- sagan-site/src/it/java/saganx/AbstractIntegrationTests.java
+++ sagan-site/src/it/java/saganx/AbstractIntegrationTests.java
@@ -1,6 +1,5 @@
 package saganx;
 
-import org.springframework.test.context.ActiveProfiles;
 import sagan.SaganProfiles;
 import sagan.blog.support.BlogService;
 import sagan.support.github.StubGithubRestClient;
@@ -10,12 +9,13 @@ import org.junit.Before;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.ConfigFileApplicationContextInitializer;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.cache.CacheManager;
 import org.springframework.security.web.FilterChainProxy;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,12 +24,11 @@ import org.springframework.web.context.WebApplicationContext;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static sagan.support.SecurityRequestPostProcessors.*;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
-@ContextConfiguration(classes = { IntegrationTestsConfig.class, InMemoryElasticSearchConfig.class },
-                      initializers = ConfigFileApplicationContextInitializer.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+@ContextConfiguration(classes = { IntegrationTestsConfig.class, InMemoryElasticSearchConfig.class })
 @Transactional
-@ActiveProfiles(profiles = {SaganProfiles.STANDALONE})
+@ActiveProfiles(profiles = { SaganProfiles.STANDALONE })
 public abstract class AbstractIntegrationTests {
 
     @Autowired
