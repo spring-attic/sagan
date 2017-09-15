d99958262b131244a4f9ed2c032a715bc849dec7
Brian Clozel <bclozel@pivotal.io>
2017-09-15 17:23:06 +0200

    Upgrade to Spring Boot 1.5.6

--- sagan-site/src/it/java/sagan/support/cache/CachedRestClientTests.java
+++ sagan-site/src/it/java/sagan/support/cache/CachedRestClientTests.java
@@ -1,27 +1,25 @@
 package sagan.support.cache;
 
-import sagan.SiteApplication;
-import sagan.support.Fixtures;
-import sagan.support.SetSystemProperty;
-import sagan.support.cache.CachedRestClientTests.TestConfig;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import sagan.SiteApplication;
+import sagan.support.Fixtures;
+import sagan.support.SetSystemProperty;
+import sagan.support.cache.CachedRestClientTests.TestConfig;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.ConfigFileApplicationContextInitializer;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
 import org.springframework.social.github.api.GitHub;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.client.RestOperations;
@@ -32,7 +30,10 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Matchers.anyObject;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.startsWith;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -41,118 +42,119 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * {@link CachedRestClient}.
  */
 @Ignore // see #342
-@RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
-@ContextConfiguration(classes = TestConfig.class, initializers = ConfigFileApplicationContextInitializer.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = TestConfig.class)
 public class CachedRestClientTests {
 
-    @ClassRule
-    public static SetSystemProperty timeToLive = new SetSystemProperty(CachedRestClient.CACHE_TTL_KEY, "1");
+	@ClassRule
+	public static SetSystemProperty timeToLive = new SetSystemProperty(CachedRestClient.CACHE_TTL_KEY, "1");
 
-    @Autowired
-    protected WebApplicationContext wac;
+	@Autowired
+	protected WebApplicationContext wac;
 
-    protected MockMvc mockMvc;
-    private RestOperations restOperations;
+	protected MockMvc mockMvc;
 
-    @Before
-    public void setupMockMvc() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
-    }
+	private RestOperations restOperations;
 
-    /**
-     * Not marked with @Configuration so as to avoid accidental component-scanning by
-     * {@link SiteApplication} in other tests.
-     */
-    @Import(SiteApplication.class)
-    public static class TestConfig {
-        @Bean
-        @Primary
-        public GitHub gitHub() {
-            return mock(GitHub.class);
-        }
+	@Before
+	public void setupMockMvc() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+	}
 
-        @Bean
-        @Primary
-        public RestTemplate restTemplate() {
-            return mock(RestTemplate.class);
-        }
-    }
+	/**
+	 * Not marked with @Configuration so as to avoid accidental component-scanning by
+	 * {@link SiteApplication} in other tests.
+	 */
+	@Import(SiteApplication.class)
+	public static class TestConfig {
+		@Bean
+		@Primary
+		public GitHub gitHub() {
+			return mock(GitHub.class);
+		}
 
-    @Autowired
-    private GitHub gitHub;
+		@Bean
+		@Primary
+		public RestTemplate restTemplate() {
+			return mock(RestTemplate.class);
+		}
+	}
 
-    @Autowired
-    private CacheManager cacheManager;
+	@Autowired
+	private GitHub gitHub;
 
-    @Autowired
-    private RestTemplate restTemplate;
+	@Autowired
+	private CacheManager cacheManager;
 
-    @SuppressWarnings("unchecked")
-    @Before
-    public void setUp() throws Exception {
-        reset(gitHub);
-        reset(restTemplate);
+	@Autowired
+	private RestTemplate restTemplate;
 
-        String requestPath = "https://api.github.com/orgs/spring-guides/repos";
+	@SuppressWarnings("unchecked")
+	@Before
+	public void setUp() throws Exception {
+		reset(gitHub);
+		reset(restTemplate);
 
-        restOperations = mock(RestOperations.class);
-        given(gitHub.restOperations()).willReturn(restOperations);
-        given(restOperations.getForObject(
-                startsWith(requestPath), (Class<String>) anyObject())).willReturn(Fixtures.githubRepoListJson());
-    }
+		String requestPath = "https://api.github.com/orgs/spring-guides/repos";
 
-    @After
-    public void tearDown() throws Exception {
-        for (String name : cacheManager.getCacheNames()) {
-            cacheManager.getCache(name).clear();
-        }
-    }
+		restOperations = mock(RestOperations.class);
+		given(gitHub.restOperations()).willReturn(restOperations);
+		given(restOperations.getForObject(
+				startsWith(requestPath), (Class<String>) anyObject())).willReturn(Fixtures.githubRepoListJson());
+	}
 
-    @Test
-    public void githubRequestsAreCached() throws Exception {
-        mockMvc.perform(get("/guides")).andExpect(status().isOk());
-        mockMvc.perform(get("/guides")).andExpect(status().isOk());
-        verify(restOperations, times(1)).getForObject(anyString(), (Class<?>) anyObject());
-    }
+	@After
+	public void tearDown() throws Exception {
+		for (String name : cacheManager.getCacheNames()) {
+			cacheManager.getCache(name).clear();
+		}
+	}
 
-    @Test
-    public void cachedItemsHaveATimeToLive() throws Exception {
-        mockMvc.perform(get("/guides")).andExpect(status().isOk());
-        verify(restOperations).getForObject(anyString(), (Class<?>) anyObject());
+	@Test
+	public void githubRequestsAreCached() throws Exception {
+		mockMvc.perform(get("/guides")).andExpect(status().isOk());
+		mockMvc.perform(get("/guides")).andExpect(status().isOk());
+		verify(restOperations, times(1)).getForObject(anyString(), (Class<?>) anyObject());
+	}
 
-        Thread.sleep(1500);
+	@Test
+	public void cachedItemsHaveATimeToLive() throws Exception {
+		mockMvc.perform(get("/guides")).andExpect(status().isOk());
+		verify(restOperations).getForObject(anyString(), (Class<?>) anyObject());
 
-        mockMvc.perform(get("/guides")).andExpect(status().isOk());
-        verify(restOperations, times(2)).getForObject(anyString(), (Class<?>) anyObject());
-    }
+		Thread.sleep(1500);
 
-    @Test
-    public void toolsSTSXmlRequestsAreCached() throws Exception {
-        String stsDownloads = Fixtures.load("/fixtures/tools/sts_downloads.xml");
-        given(
-                restTemplate.getForObject(
-                        "http://dist.springsource.com/release/STS/index-new.xml",
-                        String.class)).willReturn(stsDownloads);
+		mockMvc.perform(get("/guides")).andExpect(status().isOk());
+		verify(restOperations, times(2)).getForObject(anyString(), (Class<?>) anyObject());
+	}
 
-        mockMvc.perform(get("/tools")).andExpect(status().isOk());
-        mockMvc.perform(get("/tools")).andExpect(status().isOk());
+	@Test
+	public void toolsSTSXmlRequestsAreCached() throws Exception {
+		String stsDownloads = Fixtures.load("/fixtures/tools/sts_downloads.xml");
+		given(
+				restTemplate.getForObject(
+						"http://dist.springsource.com/release/STS/index-new.xml",
+						String.class)).willReturn(stsDownloads);
 
-        verify(restTemplate, times(1)).getForObject(anyString(), (Class<?>) anyObject());
-    }
+		mockMvc.perform(get("/tools")).andExpect(status().isOk());
+		mockMvc.perform(get("/tools")).andExpect(status().isOk());
 
-    @Test
-    public void toolsEclipseXmlRequestsAreCached() throws Exception {
-        String eclipse = Fixtures.load("/fixtures/tools/eclipse.xml");
-        given(
-                restTemplate.getForObject(
-                        "http://dist.springsource.com/release/STS/eclipse.xml",
-                        String.class)).willReturn(eclipse);
+		verify(restTemplate, times(1)).getForObject(anyString(), (Class<?>) anyObject());
+	}
 
-        mockMvc.perform(get("/tools/eclipse")).andExpect(status().isOk());
-        mockMvc.perform(get("/tools/eclipse")).andExpect(status().isOk());
+	@Test
+	public void toolsEclipseXmlRequestsAreCached() throws Exception {
+		String eclipse = Fixtures.load("/fixtures/tools/eclipse.xml");
+		given(
+				restTemplate.getForObject(
+						"http://dist.springsource.com/release/STS/eclipse.xml",
+						String.class)).willReturn(eclipse);
 
-        verify(restTemplate, times(1)).getForObject(anyString(), (Class<?>) anyObject());
-    }
+		mockMvc.perform(get("/tools/eclipse")).andExpect(status().isOk());
+		mockMvc.perform(get("/tools/eclipse")).andExpect(status().isOk());
+
+		verify(restTemplate, times(1)).getForObject(anyString(), (Class<?>) anyObject());
+	}
 
 }
--- sagan-site/src/main/java/sagan/MvcConfig.java
+++ sagan-site/src/main/java/sagan/MvcConfig.java
@@ -1,18 +1,23 @@
 package sagan;
 
-import com.github.mxab.thymeleaf.extras.dataattribute.dialect.DataAttributeDialect;
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import sagan.support.ResourceNotFoundException;
+import sagan.support.StaticPagePathFinder;
+import sagan.support.nav.Navigation;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Scope;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -26,15 +31,6 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import org.springframework.web.servlet.resource.ResourceUrlEncodingFilter;
 import org.springframework.web.servlet.resource.VersionResourceResolver;
 import org.springframework.web.util.UrlPathHelper;
-import sagan.projects.support.ProjectMetadataService;
-import sagan.support.ResourceNotFoundException;
-import sagan.support.StaticPagePathFinder;
-import sagan.support.nav.Navigation;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Properties;
 
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
@@ -44,113 +40,103 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
  */
 abstract class MvcConfig extends WebMvcConfigurerAdapter {
 
-    @Autowired
-    private StaticPagePathFinder staticPagePathFinder;
+	@Autowired
+	private StaticPagePathFinder staticPagePathFinder;
 
-    @Bean(name = {"uih", "viewRenderingHelper"})
-    @Scope("request")
-    public ViewRenderingHelper viewRenderingHelper() {
-        return new ViewRenderingHelper();
-    }
+	@Bean(name = {"uih", "viewRenderingHelper"})
+	@Scope("request")
+	public ViewRenderingHelper viewRenderingHelper() {
+		return new ViewRenderingHelper();
+	}
 
-    public abstract AppVersionHelper appVersionHelper();
+	public abstract AppVersionHelper appVersionHelper();
 
-    @ExceptionHandler
-    @ResponseStatus(NOT_FOUND)
-    public void handleException(ResourceNotFoundException ex) {
-    }
+	@ExceptionHandler
+	@ResponseStatus(NOT_FOUND)
+	public void handleException(ResourceNotFoundException ex) {
+	}
 
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        try {
-            for (StaticPagePathFinder.PagePaths paths : staticPagePathFinder.findPaths()) {
-                String urlPath = paths.getUrlPath();
-                registry.addViewController(urlPath).setViewName("pages" + paths.getFilePath());
-                if (!urlPath.isEmpty()) {
-                    registry.addViewController(urlPath + "/").setViewName("pages" + paths.getFilePath());
-                }
-            }
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		try {
+			for (StaticPagePathFinder.PagePaths paths : staticPagePathFinder.findPaths()) {
+				String urlPath = paths.getUrlPath();
+				registry.addViewController(urlPath).setViewName("pages" + paths.getFilePath());
+				if (!urlPath.isEmpty()) {
+					registry.addViewController(urlPath + "/").setViewName("pages" + paths.getFilePath());
+				}
+			}
 
-        } catch (IOException e) {
-            throw new RuntimeException("Unable to locate static pages: " + e.getMessage(), e);
-        }
-    }
+		}
+		catch (IOException e) {
+			throw new RuntimeException("Unable to locate static pages: " + e.getMessage(), e);
+		}
+	}
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new HandlerInterceptorAdapter() {
-            @Override
-            public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-                                   ModelAndView modelAndView) throws Exception {
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new HandlerInterceptorAdapter() {
+			@Override
+			public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+					ModelAndView modelAndView) throws Exception {
 
-                if (handler instanceof HandlerMethod) {
-                    HandlerMethod handlerMethod = (HandlerMethod) handler;
-                    Navigation navSection = handlerMethod.getBean().getClass().getAnnotation(Navigation.class);
-                    if (navSection != null && modelAndView != null) {
-                        modelAndView.addObject("navSection", navSection.value().toString().toLowerCase());
-                    }
-                }
-            }
-        });
-    }
+				if (handler instanceof HandlerMethod) {
+					HandlerMethod handlerMethod = (HandlerMethod) handler;
+					Navigation navSection = handlerMethod.getBean().getClass().getAnnotation(Navigation.class);
+					if (navSection != null && modelAndView != null) {
+						modelAndView.addObject("navSection", navSection.value().toString().toLowerCase());
+					}
+				}
+			}
+		});
+	}
 
-    @Configuration
-    public static class ErrorConfig implements EmbeddedServletContainerCustomizer {
+	static class ViewRenderingHelper {
 
-        @Override
-        public void customize(ConfigurableEmbeddedServletContainer factory) {
-            factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/404"));
-            factory.addErrorPages(new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500"));
-        }
+		private final UrlPathHelper urlPathHelper = new UrlPathHelper();
 
-    }
+		private HttpServletRequest request;
 
-    static class ViewRenderingHelper {
+		@Autowired
+		public void setRequest(HttpServletRequest request) {
+			this.request = request;
+		}
 
-        private final UrlPathHelper urlPathHelper = new UrlPathHelper();
+		public String navClass(String active, String current) {
+			if (active.equals(current)) {
+				return "navbar-link active";
+			}
+			else {
+				return "navbar-link";
+			}
+		}
 
-        private HttpServletRequest request;
+		public String blogClass(String active, String current) {
+			if (active.equals(current)) {
+				return "blog-category active";
+			}
+			else {
+				return "blog-category";
+			}
+		}
 
-        @Autowired
-        public void setRequest(HttpServletRequest request) {
-            this.request = request;
-        }
+		public String path() {
+			return urlPathHelper.getPathWithinApplication(request);
+		}
+	}
 
-        public String navClass(String active, String current) {
-            if (active.equals(current)) {
-                return "navbar-link active";
-            }
-            else {
-                return "navbar-link";
-            }
-        }
+	static class AppVersionHelper {
 
-        public String blogClass(String active, String current) {
-            if (active.equals(current)) {
-                return "blog-category active";
-            }
-            else {
-                return "blog-category";
-            }
-        }
+		private final String version;
 
-        public String path() {
-            return urlPathHelper.getPathWithinApplication(request);
-        }
-    }
+		public AppVersionHelper(String version) {
+			this.version = version;
+		}
 
-    static class AppVersionHelper {
-
-        private final String version;
-
-        public AppVersionHelper(String version) {
-            this.version = version;
-        }
-
-        public String version() {
-            return version;
-        }
-    }
+		public String version() {
+			return version;
+		}
+	}
 }
 
 @Configuration
@@ -158,45 +144,46 @@ abstract class MvcConfig extends WebMvcConfigurerAdapter {
 @Profile("cloudfoundry")
 class CloudFoundryMvcConfig extends MvcConfig {
 
-    @Value("${spring.git.properties:classpath:git.properties}")
-    private Resource gitProperties;
+	@Value("${spring.git.properties:classpath:git.properties}")
+	private Resource gitProperties;
 
-    public String getGitCommitId() {
-        try {
-            if (this.gitProperties.exists()) {
-                Properties properties = PropertiesLoaderUtils.loadProperties(this.gitProperties);
-                return properties.getProperty("git.commit.id.abbrev");
-            }
-        } catch (IOException exc) {
+	public String getGitCommitId() {
+		try {
+			if (this.gitProperties.exists()) {
+				Properties properties = PropertiesLoaderUtils.loadProperties(this.gitProperties);
+				return properties.getProperty("git.commit.id.abbrev");
+			}
+		}
+		catch (IOException exc) {
 
-        }
-        throw new IllegalStateException("Missing git.properties file on classpath");
-    }
+		}
+		throw new IllegalStateException("Missing git.properties file on classpath");
+	}
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-        VersionResourceResolver versionResourceResolver = new VersionResourceResolver()
-                .addFixedVersionStrategy(getGitCommitId(), "/app/**")
-                .addContentVersionStrategy("/**");
+		VersionResourceResolver versionResourceResolver = new VersionResourceResolver()
+				.addFixedVersionStrategy(getGitCommitId(), "/app/**")
+				.addContentVersionStrategy("/**");
 
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/static/")
-                .resourceChain(true)
-                .addResolver(versionResourceResolver);
+		registry.addResourceHandler("/**")
+				.addResourceLocations("classpath:/static/")
+				.resourceChain(true)
+				.addResolver(versionResourceResolver);
 
-    }
+	}
 
-    @Bean
-    public ResourceUrlEncodingFilter resourceUrlEncodingFilter() {
-        return new ResourceUrlEncodingFilter();
-    }
+	@Bean
+	public ResourceUrlEncodingFilter resourceUrlEncodingFilter() {
+		return new ResourceUrlEncodingFilter();
+	}
 
-    @Bean(name = "saganApp")
-    @Override
-    public AppVersionHelper appVersionHelper() {
-        return new AppVersionHelper(getGitCommitId());
-    }
+	@Bean(name = "saganApp")
+	@Override
+	public AppVersionHelper appVersionHelper() {
+		return new AppVersionHelper(getGitCommitId());
+	}
 }
 
 @Configuration
@@ -204,9 +191,9 @@ class CloudFoundryMvcConfig extends MvcConfig {
 @Profile("standalone")
 class StandaloneMvcConfig extends MvcConfig {
 
-    @Bean(name = "saganApp")
-    @Override
-    public AppVersionHelper appVersionHelper() {
-        return new AppVersionHelper("");
-    }
+	@Bean(name = "saganApp")
+	@Override
+	public AppVersionHelper appVersionHelper() {
+		return new AppVersionHelper("");
+	}
 }
