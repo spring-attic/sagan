9953c3f8c6eb02dc8226f6830ced48e91a17d0fb
Karine Pires <kpires@pivotal.io>
2017-09-29 18:03:56 +0200

    Add sidebar to project page.

--- sagan-site/src/main/java/sagan/projects/support/ProjectController.java
+++ sagan-site/src/main/java/sagan/projects/support/ProjectController.java
@@ -1,16 +1,15 @@
 package sagan.projects.support;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import sagan.support.nav.Navigation;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import sagan.support.nav.Navigation;
 import sagan.support.nav.Section;
 
-import static org.springframework.web.bind.annotation.RequestMethod.*;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.HEAD;
 
 /**
  * Controller that handles requests for the projects overview page at /projects.
@@ -30,6 +29,7 @@ class ProjectController {
     @RequestMapping(value = "/{projectName}", method = { GET, HEAD })
     public String showProject(Model model, @PathVariable String projectName) {
         model.addAttribute("project", projectMetadataService.getProject(projectName));
+        model.addAttribute("projects", projectMetadataService.getProjectsForCategory("active"));
         return "projects/show";
     }
 
