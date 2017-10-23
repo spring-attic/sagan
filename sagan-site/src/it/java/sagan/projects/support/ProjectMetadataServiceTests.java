36330e5d62cf1a92cdabd573bd2ac04514a71a69
Brian Clozel <bclozel@pivotal.io>
2017-10-23 18:28:06 +0200

    Remove reactor projects from fixtures

--- sagan-site/src/it/java/sagan/projects/support/ProjectMetadataServiceTests.java
+++ sagan-site/src/it/java/sagan/projects/support/ProjectMetadataServiceTests.java
@@ -22,7 +22,7 @@ public class ProjectMetadataServiceTests extends AbstractIntegrationTests {
     @Test
     public void categoriesHaveListOfProjects() throws IOException {
         List<Project> active = service.getProjectsForCategory("active");
-        assertThat(active.size(), equalTo(69));
+        assertThat(active.size(), equalTo(67));
     }
 
     @Test
