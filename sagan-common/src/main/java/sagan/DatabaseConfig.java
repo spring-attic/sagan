d10224ea13b5fc1dce15e8d80119987b53bc6e43
Brian Clozel <bclozel@pivotal.io>
2018-11-30 12:02:03 +0100

    Upgrade to Spring Boot 1.5.18.RELEASE

--- sagan-common/src/main/java/sagan/DatabaseConfig.java
+++ sagan-common/src/main/java/sagan/DatabaseConfig.java
@@ -7,25 +7,10 @@ import org.springframework.cloud.CloudFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.util.Assert;
-
-public abstract class DatabaseConfig {
-
-    @Bean
-    public abstract DataSource dataSource();
-
-    protected void configureDataSource(org.apache.tomcat.jdbc.pool.DataSource dataSource) {
-        dataSource.setMaxActive(20);
-        dataSource.setMaxIdle(8);
-        dataSource.setMinIdle(8);
-        dataSource.setTestOnBorrow(false);
-        dataSource.setTestOnReturn(false);
-    }
-}
 
 @Configuration
 @Profile(SaganProfiles.CLOUDFOUNDRY)
-class CloudFoundryDatabaseConfig extends DatabaseConfig {
+class CloudFoundryDatabaseConfig {
 
     @Bean
     public Cloud cloud() {
@@ -35,8 +20,6 @@ class CloudFoundryDatabaseConfig extends DatabaseConfig {
     @Bean
     public DataSource dataSource() {
         DataSource dataSource = cloud().getServiceConnector("sagan-db", DataSource.class, null);
-        Assert.isInstanceOf(org.apache.tomcat.jdbc.pool.DataSource.class, dataSource);
-        configureDataSource((org.apache.tomcat.jdbc.pool.DataSource) dataSource);
         return dataSource;
     }
 }
--- sagan-site/src/main/java/sagan/SiteApplication.java
+++ sagan-site/src/main/java/sagan/SiteApplication.java
@@ -4,23 +4,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import io.searchbox.client.JestClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.springframework.boot.actuate.health.AbstractHealthIndicator;
-import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.actuate.health.HealthIndicator;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.social.SocialWebAutoConfiguration;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
 import sagan.search.support.SearchService;
 import sagan.support.TuckeyRewriteFilter;
 import sagan.support.health.ElasticsearchHealthIndicator;
 
-import javax.sql.DataSource;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.social.SocialWebAutoConfiguration;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * The entry point for the Sagan web application.
@@ -30,8 +24,6 @@ import javax.sql.DataSource;
  * {@link SecurityConfig} are included as well.
  */
 @SpringBootApplication(exclude = SocialWebAutoConfiguration.class)
-@EntityScan
-@EnableJpaRepositories
 public class SiteApplication {
 
     public static final String REWRITE_FILTER_NAME = "rewriteFilter";
@@ -40,28 +32,6 @@ public class SiteApplication {
 
     public static void main(String[] args) {
         new SaganApplication(SiteApplication.class).run(args);
-    }
-
-
-    @Bean
-    public HealthIndicator dataSourceHealth(DataSource dataSource) {
-        if (dataSource instanceof org.apache.tomcat.jdbc.pool.DataSource) {
-            org.apache.tomcat.jdbc.pool.DataSource tcDataSource =
-                    (org.apache.tomcat.jdbc.pool.DataSource) dataSource;
-            return new AbstractHealthIndicator() {
-                @Override
-                protected void doHealthCheck(Health.Builder healthBuilder) throws Exception {
-                    healthBuilder.up().withDetail("active", tcDataSource.getActive())
-                            .withDetail("max_active", tcDataSource.getMaxActive())
-                            .withDetail("idle", tcDataSource.getIdle())
-                            .withDetail("max_idle", tcDataSource.getMaxIdle())
-                            .withDetail("min_idle", tcDataSource.getMinIdle())
-                            .withDetail("wait_count", tcDataSource.getWaitCount())
-                            .withDetail("max_wait", tcDataSource.getMaxWait());
-                }
-            };
-        }
-        return null;
     }
 
     @Bean
