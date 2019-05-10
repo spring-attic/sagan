61e16f70449211f032e8a169eccd58a94df8e494
Brian Clozel <bclozel@pivotal.io>
2019-05-10 11:32:24 +0200

    Fix guide references in project pages

--- sagan-renderer/src/main/java/sagan/renderer/AsciidoctorConfig.java
+++ sagan-renderer/src/main/java/sagan/renderer/AsciidoctorConfig.java
@@ -4,19 +4,14 @@ import org.asciidoctor.Asciidoctor;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
 
 /**
- * Lazily create the Asciidoctor engine
+ * Create the Asciidoctor engine
  */
 @Configuration
 public class AsciidoctorConfig {
 
 	@Bean
-	@Lazy
-	@Scope(proxyMode = ScopedProxyMode.INTERFACES)
 	public Asciidoctor asciidoctor() {
 		return Asciidoctor.Factory.create();
 	}
