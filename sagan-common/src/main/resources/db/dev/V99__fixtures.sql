2ad759fa0fa3ffa012f9ef274909021b2dc4fc10
Alex Boyko <aboyko@pivotal.io>
2019-06-21 15:09:28 -0400

    Point to Theia 1.8.0 Spring Boot extension

--- sagan-common/src/main/resources/db/dev/V99__fixtures.sql
+++ sagan-common/src/main/resources/db/dev/V99__fixtures.sql
@@ -137,4 +137,4 @@ INSERT INTO spring_tools_platform_downloads (spring_tools_platform_id, variant, 
 ('eclipse', 'macos', 'macOS 64-bit', 'http://download.springsource.com/release/STS4/4.1.1.RELEASE/dist/e4.10/spring-tool-suite-4-4.1.1.RELEASE-e4.10.0-macosx.cocoa.x86_64.dmg'),
 ('eclipse', 'linux', 'Linux 64-bit', 'http://download.springsource.com/release/STS4/4.1.1.RELEASE/dist/e4.10/spring-tool-suite-4-4.1.1.RELEASE-e4.10.0-linux.gtk.x86_64.tar.gz'),
 ('vscode', 'marketplace', 'VSCode Marketplace', 'https://marketplace.visualstudio.com/items?itemName=Pivotal.vscode-boot-dev-pack'),
-('theia', 'package', 'Package for Theia', 'https://registry.npmjs.org/@pivotal-tools/theia-spring-boot/-/theia-spring-boot-1.7.0.tgz');
+('theia', 'package', 'Package for Theia', 'https://registry.npmjs.org/@pivotal-tools/theia-spring-boot/-/theia-spring-boot-1.8.0.tgz');
