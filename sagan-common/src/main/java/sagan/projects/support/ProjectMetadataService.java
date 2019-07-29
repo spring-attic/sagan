48c4365b326a47ac232a582f75ab45ea1a51fb2c
Dave Syer <dsyer@pivotal.io>
2019-07-29 10:44:28 +0100

    Spring Boot 2.x migrations

--- sagan-common/src/main/java/sagan/projects/support/ProjectMetadataService.java
+++ sagan-common/src/main/java/sagan/projects/support/ProjectMetadataService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProjectMetadataService {
 
-	private static final Sort sortByDisplayOrderAndId = new Sort("displayOrder", "id");
+	private static final Sort sortByDisplayOrderAndId = Sort.by("displayOrder", "id");
 
     private ProjectMetadataRepository repository;
 
