53586d96e04d3a3b13826de139356625a581d5b0
Dave Syer <dsyer@pivotal.io>
2019-07-30 12:25:20 +0100

    Blog admin has to look explicitly for github id

--- sagan-site/src/main/java/sagan/blog/support/BlogAdminController.java
+++ sagan-site/src/main/java/sagan/blog/support/BlogAdminController.java
@@ -103,7 +103,7 @@ class BlogAdminController {
             model.addAttribute("formats", PostFormat.values());
             return "admin/blog/new";
         } else {
-            MemberProfile memberProfile = teamRepository.findById(new Long(principal.getName())).get();
+            MemberProfile memberProfile = teamRepository.findByGithubId(new Long(principal.getName()));
             try {
                 Post post = service.addPost(postForm, memberProfile.getUsername());
                 PostView postView = PostView.of(post, dateFactory);
