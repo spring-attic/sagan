3bdc0f1f6a432ccdd79e156fe5932478944f2fda
Brian Clozel <bclozel@pivotal.io>
2020-01-25 12:55:31 +0100

    Remove previous sagan client

--- sagan-client/src/feature/searchFacets/filterForm.js
+++
@@ -1,9 +0,0 @@
-var forEach = Function.prototype.call.bind(Array.prototype.forEach);
-
-module.exports = function(e){
-  forEach(e.target.elements, function(input) {
-    if (input.type == 'hidden' && input.name == '_filters') {
-      input.disabled = true;
-    }
-  });
-};
