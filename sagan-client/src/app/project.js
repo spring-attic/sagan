00490cb0f37facf7be4095a1d9659b80c159859a
Damien Vitrac <dvitrac@pivotal.io>
2020-02-03 20:41:50 +0100

    Project page: fix tabs

--- sagan-client/src/app/project.js
+++ sagan-client/src/app/project.js
@@ -1,1 +1,18 @@
 import '../css/project.css'
+import $ from 'jquery';
+
+
+$(document).ready(function () {
+
+    var tabs = $('#content-project .tab-pane');
+    var lis = $('#nav-project li');
+
+    $('#nav-project a').click(function (e) {
+        e.preventDefault();
+        tabs.removeClass('active');
+        lis.removeClass('active');
+        $(this).parent().addClass('active');
+        $($(this).attr('href')).addClass('active');
+    });
+
+})
