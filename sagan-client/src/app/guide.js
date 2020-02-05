1de4fd5bd0122a2c931fd8ab3dbcb7dc404c8f9b
Damien Vitrac <dvitrac@pivotal.io>
2020-02-05 23:10:00 +0100

    Guides: add search

--- sagan-client/src/app/guide.js
+++ sagan-client/src/app/guide.js
@@ -1,1 +1,37 @@
+import $ from 'jquery';
+import * as JsSearch from 'js-search';
 import '../css/guide.css'
+
+$(document).ready(function () {
+    var guides = [];
+    var _guides = $('div.guide-search');
+    var noResult = $('#guide-search-no-result');
+    if (_guides.length > 0) {
+        _guides.each(function (index) {
+            guides.push({
+                index: index,
+                obj: $(this),
+                str: $(this).attr('data-filterable')
+            })
+        });
+        var search = new JsSearch.Search('index');
+        search.addIndex('str');
+        search.addDocuments(guides);
+        $('#guide-search').keyup(function () {
+            noResult.removeClass('show');
+            if ($(this).val().trim() === '') {
+                _guides.removeClass('hide');
+            } else {
+                var result = search.search($(this).val());
+                _guides.addClass('hide');
+                $.each(result, function (index, value) {
+                    value.obj.removeClass('hide');
+
+                });
+                if (result.length === 0) {
+                    noResult.addClass('show');
+                }
+            }
+        })
+    }
+});
