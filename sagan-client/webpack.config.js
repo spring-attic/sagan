0e09f538217e95f86d36a2b9c179b6c55b44c0ba
Temenuzhka Vuchova <tvuchova@gmail.com>
2020-03-01 09:46:10 +0200

    fix verbose stack Error: sagan@ build: `webpack`

--- sagan-client/webpack.config.js
+++ sagan-client/webpack.config.js
@@ -104,7 +104,11 @@ module.exports = {
                 options: {
                     name: '[path][name].[ext]',
                 },
-            }
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                loader: 'url-loader?limit=100000'
+             }
         ]
     },
     optimization: {
