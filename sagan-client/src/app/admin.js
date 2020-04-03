a497897f6274a8ec8e0ac2cc422f63a56da032cd
Damien Vitrac <dvitrac@pivotal.io>
2020-04-03 15:31:30 +0200

    Add datetime picker on blog section in the admin

--- sagan-client/src/app/admin.js
+++ sagan-client/src/app/admin.js
@@ -1,3 +1,30 @@
 import 'bulma/css/bulma.css'
 import '@fortawesome/fontawesome-free/css/all.css'
+import $ from 'jquery';
+import 'jquery-datetimepicker/jquery.datetimepicker';
+import 'jquery-datetimepicker/jquery.datetimepicker.css';
+import {DateTime} from 'luxon';
 import '../css/admin.css'
+
+$.datetimepicker.setLocale('en');
+
+$.datetimepicker.setDateFormatter({
+    parseDate: function (date, format) {
+        var d = DateTime.fromFormat(date, format);
+        return d.isValid ? d.toJSDate() : false;
+    },
+    formatDate: function (date, format) {
+        return DateTime.fromJSDate(date).toFormat(format);
+    },
+});
+
+if ($('#datetimepicker').length > 0) {
+    $('#datetimepicker').datetimepicker({
+        format: 'y-MM-dd HH:mm',
+        formatDate: 'y-MM-dd',
+        formatTime: 'HH:mm',
+        closeOnDateSelect: true,
+        scrollMonth: false,
+        defaultTime: '12:00'
+    });
+}
