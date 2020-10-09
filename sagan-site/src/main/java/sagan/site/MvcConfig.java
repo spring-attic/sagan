eebfd87702a49e868b053dad711a1eba03029473
Brian Clozel <bclozel@vmware.com>
2020-10-09 14:44:42 +0200

    Refactor MVC configuration
    The Spring Boot 2.3 upgrade brings a lot of improvements.
    This commit removes the infrastructure that's not used by the
    application anymore and converts explicit code configuration with
    application properties now supported by Spring Boot.

--- sagan-site/src/main/java/sagan/site/MvcConfig.java
+++ sagan-site/src/main/java/sagan/site/MvcConfig.java
@@ -1,63 +1,29 @@
 package sagan.site;
 
 import java.io.IOException;
-import java.util.Properties;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import sagan.site.support.ResourceNotFoundException;
 import sagan.site.support.StaticPagePathFinder;
-import sagan.site.support.nav.Navigation;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.Scope;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.core.io.support.ResourcePatternResolver;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.method.HandlerMethod;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-import org.springframework.web.servlet.resource.ResourceUrlEncodingFilter;
-import org.springframework.web.servlet.resource.VersionResourceResolver;
-import org.springframework.web.util.UrlPathHelper;
-
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Site-wide MVC infrastructure configuration. See also {@link SiteApplication} where certain
  * additional web infrastructure is configured.
  */
-abstract class MvcConfig extends WebMvcConfigurerAdapter {
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
 
 	@Autowired
 	private StaticPagePathFinder staticPagePathFinder;
 
-	@Bean(name = {"uih", "viewRenderingHelper"})
-	@Scope("request")
-	public ViewRenderingHelper viewRenderingHelper() {
-		return new ViewRenderingHelper();
-	}
-
 	@Bean
 	public StaticPagePathFinder staticPagePathFinder(ResourcePatternResolver resourcePatternResolver) {
 		return new StaticPagePathFinder(resourcePatternResolver);
-	}
-
-	@ExceptionHandler
-	@ResponseStatus(NOT_FOUND)
-	public void handleException(ResourceNotFoundException ex) {
 	}
 
 	@Override
@@ -70,112 +36,10 @@ abstract class MvcConfig extends WebMvcConfigurerAdapter {
 					registry.addViewController(urlPath + "/").setViewName("pages" + paths.getFilePath());
 				}
 			}
-
 		}
 		catch (IOException e) {
 			throw new RuntimeException("Unable to locate static pages: " + e.getMessage(), e);
 		}
 	}
-
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(new HandlerInterceptorAdapter() {
-			@Override
-			public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-					ModelAndView modelAndView) throws Exception {
-
-				if (handler instanceof HandlerMethod) {
-					HandlerMethod handlerMethod = (HandlerMethod) handler;
-					Navigation navSection = handlerMethod.getBean().getClass().getAnnotation(Navigation.class);
-					if (navSection != null && modelAndView != null) {
-						modelAndView.addObject("navSection", navSection.value().toString().toLowerCase());
-					}
-				}
-			}
-		});
-	}
-
-	static class ViewRenderingHelper {
-
-		private final UrlPathHelper urlPathHelper = new UrlPathHelper();
-
-		private HttpServletRequest request;
-
-		@Autowired
-		public void setRequest(HttpServletRequest request) {
-			this.request = request;
-		}
-
-		public String navClass(String active, String current) {
-			if (active.equals(current)) {
-				return "navbar-link active";
-			}
-			else {
-				return "navbar-link";
-			}
-		}
-
-		public String blogClass(String active, String current) {
-			if (active.equals(current)) {
-				return "blog-category active";
-			}
-			else {
-				return "blog-category";
-			}
-		}
-
-		public String path() {
-			return urlPathHelper.getPathWithinApplication(request);
-		}
-	}
-
-}
-
-@Configuration
-@ControllerAdvice
-@Profile("cloudfoundry")
-class CloudFoundryMvcConfig extends MvcConfig {
-
-	@Value("${spring.git.properties:classpath:git.properties}")
-	private Resource gitProperties;
-
-	public String getGitCommitId() {
-		try {
-			if (this.gitProperties.exists()) {
-				Properties properties = PropertiesLoaderUtils.loadProperties(this.gitProperties);
-				return properties.getProperty("git.commit.id.abbrev");
-			}
-		}
-		catch (IOException exc) {
-
-		}
-		throw new IllegalStateException("Missing git.properties file on classpath");
-	}
-
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
-		VersionResourceResolver versionResourceResolver = new VersionResourceResolver()
-				.addFixedVersionStrategy(getGitCommitId(), "/app/**")
-				.addContentVersionStrategy("/**");
-
-		registry.addResourceHandler("/**")
-				.addResourceLocations("classpath:/static/")
-				.resourceChain(true)
-				.addResolver(versionResourceResolver);
-
-	}
-
-	@Bean
-	public ResourceUrlEncodingFilter resourceUrlEncodingFilter() {
-		return new ResourceUrlEncodingFilter();
-	}
-
-}
-
-@Configuration
-@ControllerAdvice
-@Profile("standalone")
-class StandaloneMvcConfig extends MvcConfig {
 
 }
--- sagan-site/src/main/java/sagan/site/support/ResourceNotFoundException.java
+++ sagan-site/src/main/java/sagan/site/support/ResourceNotFoundException.java
@@ -1,6 +1,11 @@
 package sagan.site.support;
 
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
 @SuppressWarnings("serial")
+@ResponseStatus(NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
     public ResourceNotFoundException(String message) {
