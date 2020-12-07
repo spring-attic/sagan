8022abd1118d5416f6fcf95cbe30fdee46aa77b6
Brian Clozel <bclozel@vmware.com>
2020-12-07 14:44:59 +0100

    Publish sagan-site contract stubs in build pipeline
    See gh-1009

--- concourse/scripts/build-sagan-site.sh
+++ concourse/scripts/build-sagan-site.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e -u -x
 source $(dirname $0)/common.sh
+repository=$(pwd)/distribution-repository
 chown gradle:gradle -R ./sagan-site-repo
 pushd sagan-site-repo > /dev/null
-su gradle -c "./gradlew --console plain :sagan-site:build"
+su gradle -c "./gradlew --console plain -Dorg.gradle.internal.launcher.welcomeMessageEnabled=false --no-daemon --max-workers=4 -PdeploymentRepository=${repository} :sagan-site:build :sagan-site:publishAllPublicationsToDeploymentRepository"
 popd > /dev/null
 cp -R sagan-site-repo/sagan-site/build/* build
