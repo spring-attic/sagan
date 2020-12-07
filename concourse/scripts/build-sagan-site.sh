3759b26ffd7feee92d8405f95d06dfa2f2ebcce0
Brian Clozel <bclozel@vmware.com>
2020-12-07 14:52:23 +0100

    Fix distribution repository permissions in build

--- concourse/scripts/build-sagan-site.sh
+++ concourse/scripts/build-sagan-site.sh
@@ -2,7 +2,8 @@
 set -e -u -x
 source $(dirname $0)/common.sh
 repository=$(pwd)/distribution-repository
-chown gradle:gradle -R ./sagan-site-repo
+mkdir -p ${repository}
+chown gradle:gradle -R ./sagan-site-repo ${repository}
 pushd sagan-site-repo > /dev/null
 su gradle -c "./gradlew --console plain -Dorg.gradle.internal.launcher.welcomeMessageEnabled=false --no-daemon --max-workers=4 -PdeploymentRepository=${repository} :sagan-site:build :sagan-site:publishAllPublicationsToDeploymentRepository"
 popd > /dev/null
