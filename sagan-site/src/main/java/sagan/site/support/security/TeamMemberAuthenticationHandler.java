1626748652bf4aba8a3cf102fd0b6eb77c7a599b
Brian Clozel <bclozel@vmware.com>
2021-02-02 17:47:20 +0100

    Delay user account creation

--- sagan-site/src/main/java/sagan/site/support/security/TeamMemberAuthenticationHandler.java
+++ sagan-site/src/main/java/sagan/site/support/security/TeamMemberAuthenticationHandler.java
@@ -7,6 +7,7 @@ import sagan.site.team.support.TeamService;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.client.authentication.OAuth2LoginAuthenticationToken;
 import org.springframework.stereotype.Component;
 
@@ -29,11 +30,13 @@ public class TeamMemberAuthenticationHandler {
 		Authentication authentication = success.getAuthentication();
 		if (authentication instanceof OAuth2LoginAuthenticationToken) {
 			OAuth2LoginAuthenticationToken oauth = (OAuth2LoginAuthenticationToken) authentication;
-			Integer id = oauth.getPrincipal().getAttribute("id");
-			logger.info("Authenticated user {}", oauth.getPrincipal().getAttribute("login").toString());
-			this.teamService.createOrUpdateMemberProfile(Integer.toUnsignedLong(id), oauth.getPrincipal());
+			logger.info("OAuth auth success for {}", oauth.getPrincipal().getAttribute("login").toString());
+			if(oauth.getAuthorities().stream().map(GrantedAuthority::getAuthority).anyMatch("ADMIN"::equals)) {
+				Integer id = oauth.getPrincipal().getAttribute("id");
+				logger.info("Authenticated user {}", oauth.getPrincipal().getAttribute("login").toString());
+				this.teamService.createOrUpdateMemberProfile(Integer.toUnsignedLong(id), oauth.getPrincipal());
+			}
 		}
-
 	}
 
 }
