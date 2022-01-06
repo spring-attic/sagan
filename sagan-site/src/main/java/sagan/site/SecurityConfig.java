d34fd446478fd5f28bd89a26d22dd7f207f418b3
Irvin Lara <irvinlaravillalobos@yahoo.com.mx>
2022-01-06 13:19:41 -0700

    Add security policies

--- sagan-site/src/main/java/sagan/site/SecurityConfig.java
+++ sagan-site/src/main/java/sagan/site/SecurityConfig.java
@@ -22,52 +22,64 @@ import org.springframework.security.web.authentication.DelegatingAuthenticationE
 import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private OAuth2UserService<OAuth2UserRequest, OAuth2User> userService;
+    @Autowired
+    private OAuth2UserService<OAuth2UserRequest, OAuth2User> userService;
 
-	@Autowired
-	private AuthenticationManager manager;
+    @Autowired
+    private AuthenticationManager manager;
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-				.addFilterAfter(githubBasicAuthFilter(), BasicAuthenticationFilter.class)
-				.exceptionHandling(handling -> handling.authenticationEntryPoint(entryPoint()))
-				.csrf(csrf -> csrf.ignoringAntMatchers("/api/**", "/webhook/**"))
-				.requiresChannel(channel ->
-						channel.requestMatchers(request -> request.getHeader("x-forwarded-port") != null).requiresSecure())
-				.authorizeRequests(req ->
-						req.mvcMatchers("/admin", "/admin/**").hasRole("ADMIN")
-								.mvcMatchers(HttpMethod.GET, "/api/**").permitAll()
-								.mvcMatchers("/api/**").access("hasRole('ADMIN') and hasRole('API')")
-				)
-				.oauth2Login(login -> login
-						.defaultSuccessUrl("/admin").loginPage("/signin")
-						.userInfoEndpoint(endpoint -> endpoint.userService(this.userService)))
-				.logout(logout -> logout.logoutUrl("/signout").logoutSuccessUrl("/"));
-	}
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .addFilterAfter(githubBasicAuthFilter(), BasicAuthenticationFilter.class)
+                .exceptionHandling(handling -> handling.authenticationEntryPoint(entryPoint()))
+                .csrf(csrf -> csrf.ignoringAntMatchers("/api/**", "/webhook/**"))
+                .requiresChannel(channel ->
+                        channel.requestMatchers(request -> request.getHeader("x-forwarded-port") != null).requiresSecure())
+                .authorizeRequests(req ->
+                        req.mvcMatchers("/admin", "/admin/**").hasRole("ADMIN")
+                                .mvcMatchers(HttpMethod.GET, "/api/**").permitAll()
+                                .mvcMatchers("/api/**").access("hasRole('ADMIN') and hasRole('API')")
+                )
+                .oauth2Login(login -> login
+                        .defaultSuccessUrl("/admin").loginPage("/signin")
+                        .userInfoEndpoint(endpoint -> endpoint.userService(this.userService)))
+                .logout(logout -> logout.logoutUrl("/signout").logoutSuccessUrl("/"))
+                .headers(headers -> headers
+                        .referrerPolicy(referrer -> referrer
+                                .policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.SAME_ORIGIN)
+                        )
+                        .contentSecurityPolicy(csp -> csp
+                                        .policyDirectives("script-src 'self' 'unsafe-eval' 'unsafe-inline' *.jquery.com *.vmware.com *.tiqcdn.com *.cookielaw.org *.marketo.com *.onetrust.com")
+                        )
+                        .permissionsPolicy(permissions -> permissions
+                                .policy("geolocation=(self)")
+                        )
+                );
+    }
 
-	@Bean
-	public AuthenticationEventPublisher authenticationEventPublisher
-			(ApplicationEventPublisher applicationEventPublisher) {
-		return new DefaultAuthenticationEventPublisher(applicationEventPublisher);
-	}
+    @Bean
+    public AuthenticationEventPublisher authenticationEventPublisher
+            (ApplicationEventPublisher applicationEventPublisher) {
+        return new DefaultAuthenticationEventPublisher(applicationEventPublisher);
+    }
 
-	private Filter githubBasicAuthFilter() {
-		return new BasicAuthenticationFilter(this.manager);
-	}
+    private Filter githubBasicAuthFilter() {
+        return new BasicAuthenticationFilter(this.manager);
+    }
 
-	private AuthenticationEntryPoint entryPoint() {
-		LinkedHashMap<RequestMatcher, AuthenticationEntryPoint> mapping = new LinkedHashMap<>();
-		mapping.put(new AntPathRequestMatcher("/api/**"), new Http403ForbiddenEntryPoint());
-		DelegatingAuthenticationEntryPoint result = new DelegatingAuthenticationEntryPoint(mapping);
-		result.setDefaultEntryPoint(new LoginUrlAuthenticationEntryPoint("/signin"));
-		return result;
-	}
+    private AuthenticationEntryPoint entryPoint() {
+        LinkedHashMap<RequestMatcher, AuthenticationEntryPoint> mapping = new LinkedHashMap<>();
+        mapping.put(new AntPathRequestMatcher("/api/**"), new Http403ForbiddenEntryPoint());
+        DelegatingAuthenticationEntryPoint result = new DelegatingAuthenticationEntryPoint(mapping);
+        result.setDefaultEntryPoint(new LoginUrlAuthenticationEntryPoint("/signin"));
+        return result;
+    }
 }
