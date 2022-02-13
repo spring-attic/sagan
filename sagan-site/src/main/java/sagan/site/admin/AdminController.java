db2dc0778893d8936e29de6cdd01d7ba57445da9
Jait Ramadandi Jeke <jaitramadandij@gmail.com>
2022-02-13 15:31:06 +0800

    Update @see related to AdminController

--- sagan-site/src/main/java/sagan/site/admin/AdminController.java
+++ sagan-site/src/main/java/sagan/site/admin/AdminController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 /**
  * Controller that handles requests for the root admin page. Administrative operations for
  * /blog, /team and /projects subsections are handled by their respective controllers.
- * 
- * @see sagan.site.blog.admin.BlogAdminController
- * @see sagan.site.team.admin.TeamAdminController
+ *
+ * @see sagan.site.blog.support.BlogAdminController
+ * @see sagan.site.team.support.TeamAdminController
  * @see sagan.site.projects.admin.ProjectAdminController
  */
 @Controller
